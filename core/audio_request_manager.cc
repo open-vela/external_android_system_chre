f641550cb840b531e5d4698c07a08072d8080cd7
openvela-robot <openvela-robot@xiaomi.com>
2019-02-09 02:33:52 +0800

    Snap for 5180536 from 6770ba0de7f479c9bd245e127ff3ddbb489ed583 to pi-platform-release
    Change-Id: I8b30f72dc5ad9657d59d7bb1a76868d6cee1d7db

--- core/audio_request_manager.cc
+++ core/audio_request_manager.cc
@@ -39,9 +39,7 @@
 
 namespace chre {
 
-void AudioRequestManager::init() {
-  mPlatformAudio.init();
-
+AudioRequestManager::AudioRequestManager() {
   size_t sourceCount = mPlatformAudio.getSourceCount();
   if (!mAudioRequestLists.reserve(sourceCount)) {
     FATAL_ERROR_OOM();
@@ -50,6 +48,10 @@ void AudioRequestManager::init() {
   for (size_t i = 0; i < sourceCount; i++) {
     mAudioRequestLists.emplace_back();
   }
+}
+
+void AudioRequestManager::init() {
+  mPlatformAudio.init();
 }
 
 bool AudioRequestManager::configureSource(const Nanoapp *nanoapp,
@@ -105,14 +107,14 @@ void AudioRequestManager::handleAudioAvailability(uint32_t handle, bool availabl
   }
 }
 
-void AudioRequestManager::logStateToBuffer(char *buffer, size_t *bufferPos,
+bool AudioRequestManager::logStateToBuffer(char *buffer, size_t *bufferPos,
                                            size_t bufferSize) const {
-  debugDumpPrint(buffer, bufferPos, bufferSize, "\nAudio:\n");
+  bool success = debugDumpPrint(buffer, bufferPos, bufferSize, "\nAudio:\n");
   for (size_t i = 0; i < mAudioRequestLists.size(); i++) {
     uint32_t handle = static_cast<uint32_t>(i);
     struct chreAudioSource source;
     mPlatformAudio.getAudioSource(handle, &source);
-    debugDumpPrint(buffer, bufferPos, bufferSize,
+    success &= debugDumpPrint(buffer, bufferPos, bufferSize,
         " handle=%" PRIu32 ", name=\"%s\", sampleRate=%" PRIu32
         ", buffer(ms)=[%" PRIu64 ",%" PRIu64 "], format=%" PRIu8 "\n",
         handle, source.name, source.sampleRate,
@@ -122,7 +124,7 @@ void AudioRequestManager::logStateToBuffer(char *buffer, size_t *bufferPos,
 
     for (const auto& request : mAudioRequestLists[i].requests) {
       for (const auto& instanceId : request.instanceIds) {
-        debugDumpPrint(buffer, bufferPos, bufferSize,
+        success &= debugDumpPrint(buffer, bufferPos, bufferSize,
             "  nanoappId=%" PRIu32 ", numSamples=%" PRIu32
             ", interval(ms)=%" PRIu64 "\n", instanceId, request.numSamples,
             Milliseconds(Nanoseconds(request.deliveryInterval))
@@ -130,6 +132,8 @@ void AudioRequestManager::logStateToBuffer(char *buffer, size_t *bufferPos,
       }
     }
   }
+
+  return success;
 }
 
 bool AudioRequestManager::validateConfigureSourceArguments(
--- platform/slpi/platform_nanoapp.cc
+++ platform/slpi/platform_nanoapp.cc
@@ -126,6 +126,12 @@ bool PlatformNanoappBase::copyNanoappFragment(
   return success;
 }
 
+void PlatformNanoappBase::loadFromFile(uint64_t appId, const char *filename) {
+  CHRE_ASSERT(!isLoaded());
+  mExpectedAppId = appId;
+  mFilename = filename;
+}
+
 void PlatformNanoappBase::loadStatic(const struct chreNslNanoappInfo *appInfo) {
   CHRE_ASSERT(!isLoaded());
   mIsStatic = true;
@@ -143,7 +149,6 @@ bool PlatformNanoappBase::isUimgApp() const {
 
 void PlatformNanoappBase::closeNanoapp() {
   if (mDsoHandle != nullptr) {
-    mAppInfo = nullptr;
     if (dlclose(mDsoHandle) != 0) {
       LOGE("dlclose failed: %s", dlerror());
     }
@@ -156,6 +161,8 @@ bool PlatformNanoappBase::openNanoapp() {
 
   if (mIsStatic) {
     success = true;
+  } else if (mFilename != nullptr) {
+    success = openNanoappFromFile();
   } else if (mAppBinary != nullptr) {
     success = openNanoappFromBuffer();
   } else {
@@ -197,9 +204,9 @@ bool PlatformNanoappBase::openNanoappFromBuffer() {
         mAppInfo = nullptr;
       } else {
         LOGI("Successfully loaded nanoapp: %s (0x%016" PRIx64 ") version 0x%"
-             PRIx32 " (%s) uimg %d system %d", mAppInfo->name, mAppInfo->appId,
-             mAppInfo->appVersion, getAppVersionString(),
-             mAppInfo->isTcmNanoapp, mAppInfo->isSystemNanoapp);
+             PRIx32 " uimg %d system %d", mAppInfo->name, mAppInfo->appId,
+             mAppInfo->appVersion, mAppInfo->isTcmNanoapp,
+             mAppInfo->isSystemNanoapp);
         memoryFreeBigImage(mAppBinary);
         mAppBinary = nullptr;
       }
@@ -209,25 +216,39 @@ bool PlatformNanoappBase::openNanoappFromBuffer() {
   return success;
 }
 
-const char *PlatformNanoappBase::getAppVersionString() const {
-  const char *versionString = "<undefined>";
-  if (mAppInfo != nullptr && mAppInfo->structMinorVersion >= 2) {
-    size_t appVersionStringLength = strlen(mAppInfo->appVersionString);
+bool PlatformNanoappBase::openNanoappFromFile() {
+  CHRE_ASSERT(mFilename != nullptr);
+  CHRE_ASSERT_LOG(mDsoHandle == nullptr, "Re-opening nanoapp");
+  bool success = false;
 
-    size_t offset = 0;
-    for (size_t i = 0; i < appVersionStringLength; i++) {
-      size_t newOffset = i + 1;
-      if (mAppInfo->appVersionString[i] == '@'
-          && newOffset < appVersionStringLength) {
-        offset = newOffset;
-        break;
+  mDsoHandle = dlopen(mFilename, RTLD_NOW);
+  if (mDsoHandle == nullptr) {
+    LOGE("Failed to load nanoapp from file %s: %s", mFilename, dlerror());
+  } else {
+    mAppInfo = static_cast<const struct chreNslNanoappInfo *>(
+        dlsym(mDsoHandle, CHRE_NSL_DSO_NANOAPP_INFO_SYMBOL_NAME));
+    if (mAppInfo == nullptr) {
+      LOGE("Failed to find app info symbol in %s: %s", mFilename, dlerror());
+    } else {
+      success = validateAppInfo(mExpectedAppId, 0, mAppInfo,
+                                true /* ignoreAppVersion */);
+      if (!success) {
+        mAppInfo = nullptr;
+      } else {
+        LOGI("Successfully loaded nanoapp %s (0x%016" PRIx64 ") version 0x%"
+             PRIx32 " uimg %d system %d from file %s", mAppInfo->name,
+             mAppInfo->appId, mAppInfo->appVersion, mAppInfo->isTcmNanoapp,
+             mAppInfo->isSystemNanoapp, mFilename);
+        // Save the app version field in case this app gets disabled and we
+        // still get a query request for the version later on. We are OK not
+        // knowing the version prior to the first load because we assume that
+        // nanoapps loaded via file are done at CHRE initialization time.
+        mExpectedAppVersion = mAppInfo->appVersion;
       }
     }
-
-    versionString = &mAppInfo->appVersionString[offset];
   }
 
-  return versionString;
+  return success;
 }
 
 uint64_t PlatformNanoapp::getAppId() const {
@@ -251,13 +272,14 @@ bool PlatformNanoapp::isSystemNanoapp() const {
   return (mAppInfo != nullptr) ? mAppInfo->isSystemNanoapp : false;
 }
 
-void PlatformNanoapp::logStateToBuffer(char *buffer, size_t *bufferPos,
+bool PlatformNanoapp::logStateToBuffer(char *buffer, size_t *bufferPos,
                                        size_t bufferSize) const {
+  bool success = true;
   if (mAppInfo != nullptr) {
-    debugDumpPrint(buffer, bufferPos, bufferSize,
-                   " %s: vendor=\"%s\" commit=\"%s\"",
-                   mAppInfo->name, mAppInfo->vendor, getAppVersionString());
+    success &= debugDumpPrint(buffer, bufferPos, bufferSize, " %s: vendor=\"%s\"",
+                              mAppInfo->name, mAppInfo->vendor);
   }
+  return success;
 }
 
 }  // namespace chre
