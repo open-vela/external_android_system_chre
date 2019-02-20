928ba556971de07f64f1ae5a063dece371c4e756
openvela-robot <openvela-robot@xiaomi.com>
2019-02-21 03:38:28 +0800

    Merge "Support big-image uncal accel/gyro/mag sensors"

--- platform/slpi/see/platform_sensor.cc
+++ platform/slpi/see/platform_sensor.cc
@@ -37,6 +37,10 @@
 #include "chre/extensions/platform/slpi/see/vendor_data_types.h"
 #endif  // CHREX_SENSOR_SUPPORT
 
+#ifdef CHRE_VARIANT_SUPPLIES_SEE_SENSORS_LIST
+#include "see_sensors.h"
+#endif  // CHRE_VARIANT_SUPPLIES_SEE_SENSORS_LIST
+
 #ifndef CHRE_SEE_NUM_TEMP_SENSORS
 // There are usually more than one 'sensor_temperature' sensors in SEE.
 // Define this in the variant-specific makefile to avoid missing sensors in
@@ -55,7 +59,33 @@ namespace {
 #error "CHRE extensions are required for micro-image SEE support"
 #endif  // CHREX_SENSOR_SUPPORT
 
-constexpr SensorType kAccelBigImageSensorType = SensorType::VendorType3;
+bool isBigImageSensorType(SensorType sensorType) {
+  return (sensorType == SensorType::VendorType3       // accel
+          || sensorType == SensorType::VendorType6    // uncal accel
+          || sensorType == SensorType::VendorType7    // uncal mag
+          || sensorType == SensorType::VendorType8);  // uncal gyro
+}
+
+/**
+ * Obtains the big-image sensor type given the specified data type and whether
+ * the sensor is runtime-calibrated or not.
+ */
+SensorType getBigImageSensorTypeFromDataType(const char *dataType,
+                                             bool calibrated) {
+  SensorType sensorType = SensorType::Unknown;
+  if (strcmp(dataType, "accel") == 0) {
+    if (calibrated) {
+      sensorType = SensorType::VendorType3;
+    } else {
+      sensorType = SensorType::VendorType6;
+    }
+  } else if (strcmp(dataType, "gyro") == 0 && !calibrated) {
+    sensorType = SensorType::VendorType7;
+  } else if (strcmp(dataType, "mag") == 0 && !calibrated) {
+    sensorType = SensorType::VendorType8;
+  }
+  return sensorType;
+}
 #endif  // CHRE_SLPI_UIMG_ENABLED
 
 //! A class that implements SeeHelperCallbackInterface.
@@ -68,6 +98,9 @@ class SeeHelperCallback : public SeeHelperCallbackInterface {
       SensorType sensorType, UniquePtr<uint8_t>&& eventData) override;
 
   void onHostWakeSuspendEvent(bool awake) override;
+
+  // TODO: Implement this
+  void onSensorBiasEvent(UniquePtr<SensorBiasData>&& biasData) override {}
 };
 
 //! A struct to facilitate sensor discovery
@@ -75,6 +108,8 @@ struct SuidAttr {
   sns_std_suid suid;
   SeeAttributes attr;
 };
+
+#ifndef CHRE_VARIANT_SUPPLIES_SEE_SENSORS_LIST
 
 //! The list of SEE platform sensor data types that CHRE intends to support.
 //! The standardized strings are defined in sns_xxx.proto.
@@ -88,6 +123,24 @@ const char *kSeeDataTypes[] = {
   "motion_detect",
   "stationary_detect",
 };
+
+#endif  // CHRE_VARIANT_SUPPLIES_SEE_SENSORS_LIST
+
+void handleMissingSensor() {
+  // Try rebooting if a sensor is missing, which might help recover from a
+  // transient failure/race condition at startup. But to avoid endless crashes,
+  // only do this within the first 45 seconds after boot - we rely on knowledge
+  // that getMonotonicTime() maps into QTimer here, and QTimer only resets when
+  // the entire system is rebooted (it continues increasing after SLPI SSR).
+#ifndef CHRE_LOG_ONLY_NO_SENSOR
+  if (SystemTime::getMonotonicTime() < Seconds(45)) {
+    FATAL_ERROR("Missing required sensor(s)");
+  } else
+#endif
+  {
+    LOGE("Missing required sensor(s)");
+  }
+}
 
 /**
  * Obtains the sensor type given the specified data type and whether the sensor
@@ -123,6 +176,8 @@ SensorType getSensorTypeFromDataType(const char *dataType, bool calibrated) {
     sensorType = SensorType::InstantMotion;
   } else if (strcmp(dataType, "stationary_detect") == 0) {
     sensorType = SensorType::StationaryDetect;
+  } else if (strcmp(dataType, "step_detect") == 0) {
+    sensorType = SensorType::StepDetect;
 #ifdef CHREX_SENSOR_SUPPORT
   } else if (strcmp(dataType, kVendorDataTypes[0]) == 0) {
     sensorType = SensorType::VendorType0;
@@ -201,10 +256,7 @@ void updateSamplingStatus(
           EventLoopManagerSingleton::get()->getSensorRequestManager()
           .getRequests(update.sensorType);
       for (const auto& req : requests) {
-        if (req.getNanoapp() != nullptr) {
-          postSamplingStatusEvent(req.getNanoapp()->getInstanceId(),
-                                  sensorHandle, newStatus);
-        }
+        postSamplingStatusEvent(req.getInstanceId(), sensorHandle, newStatus);
       }
     }
   }
@@ -325,9 +377,9 @@ void addSensor(SeeHelper& seeHelper, SensorType sensorType,
     FATAL_ERROR("Failed to allocate new sensor: out of memory");
   }
 
-  // Resample big image accel to reduce system load during sw flush.
+  // Resample big image sensors to reduce system load during sw flush.
 #ifdef CHRE_SLPI_UIMG_ENABLED
-  bool resample = (sensorType == kAccelBigImageSensorType);
+  bool resample = isBigImageSensorType(sensorType);
 #else
   bool resample = false;
 #endif
@@ -429,7 +481,7 @@ void findAndAddSensorsForType(
   DynamicVector<SuidAttr> primarySensors;
   if (!getSuidAndAttrs(seeHelper, dataType, &primarySensors,
                        1 /* minNumSuids */)) {
-    FATAL_ERROR("Failed to get primary sensor UID and attributes");
+    handleMissingSensor();
   }
 
   for (const auto& primarySensor : primarySensors) {
@@ -482,14 +534,33 @@ void findAndAddSensorsForType(
  * Registers alternate sensor(s) to be used separately by big image nanoapps.
  */
 void getBigImageSensors(DynamicVector<Sensor> *sensors) {
-  // Currently, just adding calibrated accel, as it's the one we know that big
-  // image nanoapps will need at a different batching rate compared to uimg
+  CHRE_ASSERT(sensors);
+
+  // Currently, just adding calibrated accel and uncal accel/gyro/mag as they
+  // are the ones we know that big image nanoapps will need at a different
+  // batching rate compared to uimg.
+  const char *kBigImageDataTypes[] = {
+    "accel",
+    "gyro",
+    "mag",
+  };
+
   SeeHelper& seeHelper = *getBigImageSeeHelper();
-  const char *kAccelDataType = "accel";
   DynamicVector<SuidAttr> nullTemperatureSensorList;
-  findAndAddSensorsForType(
-      seeHelper, nullTemperatureSensorList, kAccelDataType,
-      kAccelBigImageSensorType, true /* skipAdditionalTypes */, sensors);
+
+  for (size_t i = 0; i < ARRAY_SIZE(kBigImageDataTypes); i++) {
+    const char *dataType = kBigImageDataTypes[i];
+    // Loop through potential cal/uncal sensors.
+    for (size_t j = 0; j < 2; j++) {
+      SensorType sensorType = getBigImageSensorTypeFromDataType(
+          dataType, (j == 0) /* calibrated */);
+      if (sensorType != SensorType::Unknown) {
+        findAndAddSensorsForType(
+            seeHelper, nullTemperatureSensorList, dataType, sensorType,
+            true /* skipAdditionalTypes */, sensors);
+      }
+    }
+  }
 }
 #endif  // CHRE_SLPI_UIMG_ENABLED
 
@@ -535,7 +606,7 @@ bool PlatformSensor::getSensors(DynamicVector<Sensor> *sensors) {
   DynamicVector<SuidAttr> tempSensors;
   if (!getSuidAndAttrs(seeHelper, "sensor_temperature", &tempSensors,
                        CHRE_SEE_NUM_TEMP_SENSORS)) {
-      FATAL_ERROR("Failed to get temperature sensor UID and attributes");
+    handleMissingSensor();
   }
 
 #ifndef CHREX_SENSOR_SUPPORT
@@ -586,7 +657,7 @@ bool PlatformSensor::applyRequest(const SensorRequest& request) {
 
   SeeHelper *seeHelper = getSeeHelper();
 #ifdef CHRE_SLPI_UIMG_ENABLED
-  if (getSensorType() == kAccelBigImageSensorType) {
+  if (isBigImageSensorType(getSensorType())) {
     seeHelper = getBigImageSeeHelper();
     slpiForceBigImage();
   }
