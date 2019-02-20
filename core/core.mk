dcb5e31946a9f9918836af9f9db4648d683d8729
openvela-robot <openvela-robot@xiaomi.com>
2019-02-20 16:20:07 +0800

    DO NOT MERGE - Merge pi-dev@5234907 into stage-aosp-master
    Bug: 120848293
    Change-Id: I51ace150b7d91858e7cb6a4b55e3559d58ada604

--- core/core.mk
+++ core/core.mk
@@ -13,6 +13,7 @@ COMMON_SRCS += core/event.cc
 COMMON_SRCS += core/event_loop.cc
 COMMON_SRCS += core/event_loop_manager.cc
 COMMON_SRCS += core/event_ref_queue.cc
+COMMON_SRCS += core/gnss_manager.cc
 COMMON_SRCS += core/host_comms_manager.cc
 COMMON_SRCS += core/init.cc
 COMMON_SRCS += core/nanoapp.cc
@@ -22,26 +23,13 @@ COMMON_SRCS += core/sensor_request_manager.cc
 COMMON_SRCS += core/sensor_type.cc
 COMMON_SRCS += core/static_nanoapps.cc
 COMMON_SRCS += core/timer_pool.cc
+COMMON_SRCS += core/wifi_request_manager.cc
+COMMON_SRCS += core/wifi_scan_request.cc
+COMMON_SRCS += core/wwan_request_manager.cc
 
 # Optional audio support.
 ifeq ($(CHRE_AUDIO_SUPPORT_ENABLED), true)
 COMMON_SRCS += core/audio_request_manager.cc
-endif
-
-# Optional GNSS support.
-ifeq ($(CHRE_GNSS_SUPPORT_ENABLED), true)
-COMMON_SRCS += core/gnss_manager.cc
-endif
-
-# Optional Wi-Fi support.
-ifeq ($(CHRE_WIFI_SUPPORT_ENABLED), true)
-COMMON_SRCS += core/wifi_request_manager.cc
-COMMON_SRCS += core/wifi_scan_request.cc
-endif
-
-# Optional WWAN support.
-ifeq ($(CHRE_WWAN_SUPPORT_ENABLED), true)
-COMMON_SRCS += core/wwan_request_manager.cc
 endif
 
 # GoogleTest Source Files ######################################################
