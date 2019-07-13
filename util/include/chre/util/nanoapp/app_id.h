85e90f42fee80f34638d503cddad28fd2114ac51
openvela-robot <openvela-robot@xiaomi.com>
2019-07-13 23:18:34 +0800

    Snap for 5726890 from 7da462c23e60035f9d3aec0972403833b3e8a2f9 to qt-c2f2-release
    Change-Id: I6d1a2ec5e958631046a0dcac1e89e226bcb3f607

--- util/include/chre/util/nanoapp/app_id.h
+++ util/include/chre/util/nanoapp/app_id.h
@@ -79,7 +79,6 @@ constexpr uint64_t kAshWorldAppId         = makeExampleNanoappId(11);
 constexpr uint64_t kAudioWorldAppId       = makeExampleNanoappId(12);
 constexpr uint64_t kHostAwakeWorldAppId   = makeExampleNanoappId(13);
 constexpr uint64_t kAudioStressTestAppId  = makeExampleNanoappId(14);
-constexpr uint64_t kPowerTestAppId        = makeExampleNanoappId(15);
 
 }  // namespace chre
 
