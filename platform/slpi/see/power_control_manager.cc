44eab6223071f1eade06822c4375eb3a17db5cac
liujinye <liujinye@xiaomi.com>
2019-08-07 17:46:05 +0800

    Add CHRE daemon retry logic in initial time sync am: abdf6fe6d7 am: eaa2a37cf2
    Change-Id: I2fc0140228a183cf4276263e18da9f3991892960

--- platform/slpi/see/power_control_manager.cc
+++ platform/slpi/see/power_control_manager.cc
@@ -23,7 +23,7 @@
 
 namespace chre {
 
-PowerControlManagerBase::PowerControlManagerBase() {
+PowerControlManagerBase::PowerControlManagerBase() : mHostIsAwake(true) {
 #ifdef CHRE_THREAD_UTIL_ENABLED
   sns_client_create_thread_utilization_client(&mThreadUtilClient);
 #endif  // CHRE_THREAD_UTIL_ENABLED
@@ -42,6 +42,11 @@ bool PowerControlManagerBase::voteBigImage(bool bigImage) {
 void PowerControlManagerBase::onHostWakeSuspendEvent(bool awake) {
   if (mHostIsAwake != awake) {
     mHostIsAwake = awake;
+
+    if (!awake) {
+      EventLoopManagerSingleton::get()->getHostCommsManager()
+          .resetBlameForNanoappHostWakeup();
+    }
 
     EventLoopManagerSingleton::get()->getEventLoop().postEvent(
         mHostIsAwake ? CHRE_EVENT_HOST_AWAKE : CHRE_EVENT_HOST_ASLEEP,
