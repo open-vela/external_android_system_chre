9a20876a11c759b6a8aef07b7ec1db277cc1e0ab
openvela-robot <openvela-robot@xiaomi.com>
2019-08-13 13:18:08 +0800

    Respect reserved event count for postEventOrFree
    Continuous sensor data can quickly flood CHRE's event queue when several
    sensors are enabled at a max rate and CHRE stalls when issuing an
    external request for some reason.
    
    To avoid this, modify postEventOrFree to respect the
    kMinReservedHighPriorityEventCount var that reserves space for high
    priority events and migrate several methods that (mistakenly) use this
    API to the postEvent method to guarantee their event delivery.
    
    Additionally, modify the postEvent method to become postEventOrDie and
    only allow system events to be posted here which also requires moving
    nanoapp events to the new postLowPriorityEventOrFree function.
    
    Bug: 139448882
    Test: Load onto device and play w/ a_sns_test and see if crashes occur
    Test: ./load_chre, ./run_tests.sh, ./run_sim.sh
    Change-Id: I1e6d0268b96934b805aaa0dd8ee831e05f74f0f7
    Merged-In: I1e6d0268b96934b805aaa0dd8ee831e05f74f0f7

--- apps/chqts/src/general_test/send_message_to_host_test.cc
+++ apps/chqts/src/general_test/send_message_to_host_test.cc
@@ -30,6 +30,7 @@ using nanoapp_testing::sendFatalFailureToHost;
 using nanoapp_testing::sendInternalFailureToHost;
 using nanoapp_testing::sendSuccessToHost;
 
+
 /*
  * Our test essentially has nine stages.  The first eight stages all involve
  * sending data to the Host.  Here is a table describing them:
@@ -81,20 +82,19 @@ namespace general_test {
 // TODO(b/32114261): Remove this variable.
 extern bool gUseNycMessageHack;
 
-uint8_t SendMessageToHostTest::sSmallMessageData[kSmallMessageTestCount]
-                                                [kSmallMessageSize];
+uint8_t SendMessageToHostTest::sSmallMessageData[kSmallMessageTestCount][kSmallMessageSize];
 void *SendMessageToHostTest::sLargeMessageData[2];
 constexpr uint32_t SendMessageToHostTest::kLargeSizes[2];
 
 bool SendMessageToHostTest::sInMethod = false;
 uint32_t SendMessageToHostTest::sFinishedBitmask = 0;
 
-template <uint8_t kCallbackIndex>
+template<uint8_t kCallbackIndex>
 void SendMessageToHostTest::smallMessageCallback(void *message,
                                                  size_t messageSize) {
   if (sInMethod) {
-    sendFatalFailureToHost(
-        "smallMessageCallback called while another nanoapp method is running");
+    sendFatalFailureToHost("smallMessageCallback called while another "
+                           "nanoapp method is running");
   }
   sInMethod = true;
   if (message == nullptr) {
@@ -105,7 +105,7 @@ void SendMessageToHostTest::smallMessageCallback(void *message,
     sendFatalFailureToHost("smallMessageCallback given bad messageSize:",
                            &size);
   }
-  const uint8_t *msg = static_cast<const uint8_t *>(message);
+  const uint8_t *msg = static_cast<const uint8_t*>(message);
   for (size_t i = 0; i < messageSize; i++) {
     if (msg[i] != kDataByte) {
       sendFatalFailureToHost("Corrupt data in smallMessageCallback");
@@ -123,7 +123,8 @@ void SendMessageToHostTest::smallMessageCallback(void *message,
       expectedCallbackIndex = 1;
       break;
     case 2:
-      sendFatalFailureToHost("callback invoked when null callback given");
+      sendFatalFailureToHost("callback invoked when null callback "
+                             "given");
       break;
     default:
       sendInternalFailureToHost("Invalid index", &stage);
@@ -161,8 +162,8 @@ uint32_t SendMessageToHostTest::getSmallDataIndex(const uint8_t *data) {
 void SendMessageToHostTest::largeMessageCallback(void *message,
                                                  size_t messageSize) {
   if (sInMethod) {
-    sendFatalFailureToHost(
-        "largeMessageCallback called while another nanoapp method is running");
+    sendFatalFailureToHost("largeMessageCallback called while another "
+                           "nanoapp method is running");
   }
   sInMethod = true;
   if (message == nullptr) {
@@ -177,9 +178,10 @@ void SendMessageToHostTest::largeMessageCallback(void *message,
     sendFatalFailureToHost("largeMessageCallback given bad message");
   }
   if (messageSize != kLargeSizes[index]) {
-    sendFatalFailureToHost("largeMessageCallback given incorrect messageSize");
+    sendFatalFailureToHost("largeMessageCallback given incorrect "
+                           "messageSize");
   }
-  const uint8_t *msg = static_cast<const uint8_t *>(message);
+  const uint8_t *msg = static_cast<const uint8_t*>(message);
   for (size_t i = 0; i < messageSize; i++) {
     if (msg[i] != kDataByte) {
       sendFatalFailureToHost("Corrupt data in largeMessageCallback");
@@ -196,7 +198,8 @@ void SendMessageToHostTest::markSuccess(uint32_t stage) {
   chreLog(CHRE_LOG_DEBUG, "Stage %" PRIu32 " succeeded", stage);
   uint32_t finishedBit = (1 << stage);
   if (sFinishedBitmask & finishedBit) {
-    sendFatalFailureToHost("callback called multiple times for stage:", &stage);
+    sendFatalFailureToHost("callback called multiple times for stage:",
+                           &stage);
   }
   if ((kAllFinished & finishedBit) == 0) {
     sendFatalFailureToHost("markSuccess bad stage", &stage);
@@ -216,7 +219,8 @@ void SendMessageToHostTest::prepTestMemory() {
     if (sLargeMessageData[i] == nullptr) {
       sendFatalFailureToHost("Insufficient heap memory for test");
     }
-    nanoapp_testing::memset(sLargeMessageData[i], kDataByte, kLargeSizes[i]);
+    nanoapp_testing::memset(sLargeMessageData[i], kDataByte,
+                            kLargeSizes[i]);
   }
 }
 
@@ -230,8 +234,8 @@ void SendMessageToHostTest::sendMessageMaxSize() {
   //     incorrect to use while we're working around this bug.  When the
   //     bug is fixed, we'll add this declaration, and use the method
   //     widely.
-  nanoapp_testing::sendMessageToHost(MessageType::kContinue, &maxSize,
-                                     sizeof(maxSize));
+  nanoapp_testing::sendMessageToHost(MessageType::kContinue,
+                                     &maxSize, sizeof(maxSize));
 }
 
 // Wrapper for chreSendMessageToHost() that sets sInMethod to false during its
@@ -249,7 +253,9 @@ bool SendMessageToHostTest::sendMessageToHost(
   return success;
 }
 
-SendMessageToHostTest::SendMessageToHostTest() : Test(CHRE_API_VERSION_1_0) {}
+SendMessageToHostTest::SendMessageToHostTest()
+  : Test(CHRE_API_VERSION_1_0) {
+}
 
 void SendMessageToHostTest::setUp(uint32_t messageSize,
                                   const void * /* message */) {
@@ -309,8 +315,8 @@ void SendMessageToHostTest::setUp(uint32_t messageSize,
   // stage: 6
   if (sendMessageToHost(sLargeMessageData[0], kLargeSizes[0],
                         kUntestedMessageType, largeMessageCallback)) {
-    sendFatalFailureToHost(
-        "Oversized data to chreSendMessageToHost claimed success");
+    sendFatalFailureToHost("Oversized data to chreSendMessageToHost "
+                           "claimed success");
   }
 
   // stage: 7
@@ -324,10 +330,10 @@ void SendMessageToHostTest::setUp(uint32_t messageSize,
 
 void SendMessageToHostTest::handleEvent(uint32_t senderInstanceId,
                                         uint16_t eventType,
-                                        const void *eventData) {
+                                        const void* eventData) {
   if (sInMethod) {
-    sendFatalFailureToHost(
-        "handleEvent invoked while another nanoapp method is running");
+    sendFatalFailureToHost("handleEvent invoked while another nanoapp "
+                           "method is running");
   }
   sInMethod = true;
 
--- apps/wifi_offload/test/utility.cc
+++ apps/wifi_offload/test/utility.cc
@@ -160,4 +160,4 @@ void init(chreWifiScanResult &result, RandomGenerator &rand_gen) {
   init_security_mode(result.securityMode, rand_gen);
 }
 
-}  // namespace wifi_offload_test
+}  // wifioffloadtesthelper namespace
