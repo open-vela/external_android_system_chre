60c22120748c4acb4c8f604cbcaee4309c7fe690
liujinye <liujinye@xiaomi.com>
2019-12-04 17:11:48 +0800

    Support default versions of SLPI builds
    The default build for SLPI may not expose as many temp sensors or
    different sensor types than what CHRE is configured for. Modify CHRE to
    avoid fatal errors on other devices.
    
    This involves:
    
    1) Reducing the number of minimum temperature sensors from 3 -> 1
    2) Remove requirement for accel_cal sensor
    3) Changing default sensor SUID for instant motion and stationary detect
    to match names used in the default SLPI build
    4) Support decoding data from instant motion / stationary sensors
    5) Remove requirement for temperature sensors to have the same
    attributes as their primary sensor
    
    Bug: 131912580
    Test: Build SLPI and run on non-Pixel device with sensor world in
    break-it mode
    Test: Run CHQTS
    Merged-In: I06c72735cf91444a7162d5fa9ca05135bbc3b565
    Change-Id: I06c72735cf91444a7162d5fa9ca05135bbc3b565

--- core/wwan_request_manager.cc
+++ core/wwan_request_manager.cc
@@ -31,7 +31,8 @@ uint32_t WwanRequestManager::getCapabilities() {
   return mPlatformWwan.getCapabilities();
 }
 
-bool WwanRequestManager::requestCellInfo(Nanoapp *nanoapp, const void *cookie) {
+bool WwanRequestManager::requestCellInfo(Nanoapp *nanoapp,
+                                         const void *cookie) {
   CHRE_ASSERT(nanoapp);
 
   bool success = false;
@@ -52,8 +53,7 @@ bool WwanRequestManager::requestCellInfo(Nanoapp *nanoapp, const void *cookie) {
 void WwanRequestManager::handleCellInfoResult(chreWwanCellInfoResult *result) {
   auto callback = [](uint16_t /* eventType */, void *eventData) {
     auto *cellInfoResult = static_cast<chreWwanCellInfoResult *>(eventData);
-    EventLoopManagerSingleton::get()
-        ->getWwanRequestManager()
+    EventLoopManagerSingleton::get()->getWwanRequestManager()
         .handleCellInfoResultSync(cellInfoResult);
   };
 
@@ -65,29 +65,30 @@ void WwanRequestManager::handleCellInfoResultSync(
     chreWwanCellInfoResult *result) {
   if (mCellInfoRequestingNanoappInstanceId.has_value()) {
     result->cookie = mCellInfoRequestingNanoappCookie;
-    EventLoopManagerSingleton::get()->getEventLoop().postEventOrDie(
-        CHRE_EVENT_WWAN_CELL_INFO_RESULT, result, freeCellInfoResultCallback,
-        mCellInfoRequestingNanoappInstanceId.value());
+    EventLoopManagerSingleton::get()->getEventLoop()
+        .postEvent(CHRE_EVENT_WWAN_CELL_INFO_RESULT, result,
+                   freeCellInfoResultCallback, kSystemInstanceId,
+                   mCellInfoRequestingNanoappInstanceId.value());
   } else {
     LOGE("Cell info results received unexpectedly");
   }
 }
 
-void WwanRequestManager::logStateToBuffer(DebugDumpWrapper &debugDump) const {
-  debugDump.print("\nWWAN:\n");
+void WwanRequestManager::logStateToBuffer(char *buffer, size_t *bufferPos,
+                                          size_t bufferSize) const {
+  debugDumpPrint(buffer, bufferPos, bufferSize, "\nWWAN:\n");
   if (mCellInfoRequestingNanoappInstanceId.has_value()) {
-    debugDump.print(" WWAN request pending nanoappId=%" PRIu32 "\n",
-                    mCellInfoRequestingNanoappInstanceId.value());
+    debugDumpPrint(buffer, bufferPos, bufferSize,
+                   " WWAN request pending nanoappId=%" PRIu32 "\n",
+                   mCellInfoRequestingNanoappInstanceId.value());
   }
 }
 
 void WwanRequestManager::handleFreeCellInfoResult(
     chreWwanCellInfoResult *result) {
   if (mCellInfoRequestingNanoappInstanceId.has_value()) {
-    Nanoapp *nanoapp =
-        EventLoopManagerSingleton::get()
-            ->getEventLoop()
-            .findNanoappByInstanceId(*mCellInfoRequestingNanoappInstanceId);
+    Nanoapp *nanoapp = EventLoopManagerSingleton::get()->getEventLoop()
+        .findNanoappByInstanceId(*mCellInfoRequestingNanoappInstanceId);
     if (nanoapp != nullptr) {
       nanoapp->unregisterForBroadcastEvent(CHRE_EVENT_WWAN_CELL_INFO_RESULT);
     } else {
@@ -105,8 +106,7 @@ void WwanRequestManager::handleFreeCellInfoResult(
 void WwanRequestManager::freeCellInfoResultCallback(uint16_t eventType,
                                                     void *eventData) {
   auto *result = static_cast<chreWwanCellInfoResult *>(eventData);
-  EventLoopManagerSingleton::get()
-      ->getWwanRequestManager()
+  EventLoopManagerSingleton::get()->getWwanRequestManager()
       .handleFreeCellInfoResult(result);
 }
 
