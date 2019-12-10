76b4e3d373599d2c2e965c1027acae0b4dea859c
openvela-robot <openvela-robot@xiaomi.com>
2019-12-11 04:12:09 +0800

    Snap for 6063671 from b0491eb0bfeb280acefa7f98650053b07cdf59f7 to rvc-release
    Change-Id: I86aba5163d396e869c1af09a3d12cce9c29ac9d9

--- core/include/chre/core/sensor_type_helpers.h
+++ core/include/chre/core/sensor_type_helpers.h
@@ -89,12 +89,6 @@ class SensorTypeHelpers : public PlatformSensorTypeHelpers {
    *     event.
    */
   static size_t getLastEventSize(uint8_t sensorType);
-
-  /**
-   * @param sensorType The sensor type to obtain a string for.
-   * @return A string representation of the sensor type.
-   */
-  static const char *getSensorTypeName(uint8_t sensorType);
 };
 
 }  // namespace chre
