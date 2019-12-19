77dfea094a56b990d49497776fa21315967f370b
liujinye <liujinye@xiaomi.com>
2019-12-20 01:45:03 +0800

    Merge "Pull FATAL_ERROR implementation into a function"

--- apps/test/chqts/src/general_test/cell_info_wcdma.h
+++ apps/test/chqts/src/general_test/cell_info_wcdma.h
@@ -27,10 +27,9 @@ class CellInfoWcdma : private CellInfoBase {
   static bool validate(const struct chreWwanCellInfoWcdma &cell);
 
  private:
-  static bool validateIdentity(
-      const struct chreWwanCellIdentityWcdma &identity);
+  static bool validateIdentity(const struct chreWwanCellIdentityWcdma identity);
   static bool validateSignalStrength(
-      const struct chreWwanSignalStrengthWcdma &strength);
+      const struct chreWwanSignalStrengthWcdma strength);
 };
 
 }  // namespace general_test
