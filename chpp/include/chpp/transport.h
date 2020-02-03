c2b972aaaa32a8b56cec25b1acd7a429015caa5e
openvela-robot <openvela-robot@xiaomi.com>
2020-02-03 21:29:59 +0800

    Merge "Use intialized handle to configure sensor mode"

--- chpp/include/chpp/transport.h
+++ chpp/include/chpp/transport.h
@@ -25,7 +25,6 @@
 
 #include "chpp/macros.h"
 #include "chpp/memory.h"
-#include "chpp/mutex.h"
 #include "chpp/platform/log.h"
 
 #ifdef __cplusplus
@@ -179,8 +178,6 @@ struct ChppTransportState {
   struct ChppTransportHeader txHeader;  // Tx packet header
   struct ChppTransportFooter txFooter;  // Tx packet footer (checksum)
   struct ChppDatagram txDatagram;       // Tx datagram
-
-  struct ChppMutex mutex;  // Prevents corruption of this state
 };
 
 /************************************************
@@ -188,25 +185,17 @@ struct ChppTransportState {
  ***********************************************/
 
 /**
- * Initializes the CHPP transport layer state stored in the parameter context.
- *
- * @param context Is used to maintain status. Must be provided and initialized
- * through chppTransportInit for each transport layer instance. Cannot be null.
- */
-void chppTransportInit(struct ChppTransportState *context);
-
-/**
  * Processes all incoming data on the serial port based on the Rx state.
  * stream. Checks checksum, triggering the correct response (ACK / NACK).
  * Moves the state to CHPP_STATE_PREAMBLE afterwards.
  *
- * TODO: Add requirements, e.g. context must not be modified unless locked via
- * mutex.
+ * TODO: Add requirements, e.g. must not be interrupt context, must always be
+ * called from the same thread
  *
  * TODO: Add sufficient outward facing documentation
  *
- * @param context Is used to maintain status. Must be provided and initialized
- * through chppTransportInit for each transport layer instance. Cannot be null.
+ * @param context Is used to maintain status. Must be provided, zero
+ * initialized, for each transport layer instance. Cannot be null.
  * @param buf Input data. Cannot be null.
  * @param len Length of input data in bytes.
  *
@@ -214,8 +203,8 @@ void chppTransportInit(struct ChppTransportState *context);
  * preamble. This allows the driver to (optionally) filter incoming zeros and
  * save processing
  */
-bool chppRxDataCb(struct ChppTransportState *context, const uint8_t *buf,
-                  size_t len);
+bool chppRxData(struct ChppTransportState *context, const uint8_t *buf,
+                size_t len);
 
 #ifdef __cplusplus
 }
