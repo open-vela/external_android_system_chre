43b3bc41b1e952b56d7f5ac1bff8f23e42808871
openvela-robot <openvela-robot@xiaomi.com>
2020-02-13 04:25:39 +0800

    Snap for 6203617 from eff0863ef806e4d7413c61c1fd18b7278084768a to rvc-release
    Change-Id: Icd2402b0b15a2ef465825b966bade990362160f6

--- core/include/chre/core/event_loop_common.h
+++ core/include/chre/core/event_loop_common.h
@@ -50,7 +50,6 @@ enum class SystemCallbackType : uint16_t {
   SensorFlushTimeout,
   SensorStatusInfoResponse,
   DeferredMessageToNanoappFromHost,
-  SettingChangeEvent,
 };
 
 //! The function signature of a system callback mirrors the CHRE event free
