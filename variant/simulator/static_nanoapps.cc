718df051f51c8e4098e5634ac3db3c98488c631b
openvela-robot <openvela-robot@xiaomi.com>
2020-03-10 21:08:22 +0800

    Take NanoAppBinary as argument in CHRE settings tests am: e4f304bb05 am: 176f8660bc am: a57dee8704 am: 323735f5db
    Change-Id: I84ed3cca737a109f20f59c450bfb34b73f0e20bd

--- variant/simulator/static_nanoapps.cc
+++ variant/simulator/static_nanoapps.cc
@@ -24,7 +24,6 @@ namespace chre {
 const StaticNanoappInitFunction kStaticNanoappList[] = {
     // clang-format off
     initializeStaticNanoappAudioWorld,
-    initializeStaticNanoappDebugDumpWorld
     initializeStaticNanoappGnssWorld,
     initializeStaticNanoappHelloWorld,
     initializeStaticNanoappMessageWorld,
