ffd3ae31dc8ba51e3ced1bed24be7d810fd2ce73
liujinye <liujinye@xiaomi.com>
2020-04-08 02:12:49 +0800

    Snap for 6374687 from be9b6b554a456ffaf3184f392c1e206548b8d195 to rvc-release
    Change-Id: Ic7b4ecc1d56c8be05b384f01c2589a27d33e5a26

--- chpp/platform/linux/include/chpp/platform/log.h
+++ chpp/platform/linux/include/chpp/platform/log.h
@@ -27,8 +27,7 @@ extern "C" {
 #define __FILENAME__ __FILE__
 #endif
 
-// TODO: Should use PRIu8 etc. from inttypes.h instead of %d, etc. (add -Wall
-// and -Werror to cflags to catch these)
+// TODO: consider switching to a PAL interface to assist platform adaptation
 #define CHPP_LINUX_LOG(level, color, fmt, ...)                         \
   printf("\e[" color "m%s %s:%d\t" fmt "\e[0m\n", level, __FILENAME__, \
          __LINE__, ##__VA_ARGS__)
