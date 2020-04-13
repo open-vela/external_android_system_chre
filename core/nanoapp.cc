358c10a7e4a6952a49cb1b4075d93a17419bb22b
liujinye <liujinye@xiaomi.com>
2020-04-14 02:20:03 +0800

    Merge "Do not duplicate CrossValidatorState ctor" into rvc-dev

--- core/nanoapp.cc
+++ core/nanoapp.cc
@@ -21,7 +21,6 @@
 #include "chre/platform/fatal_error.h"
 #include "chre/platform/log.h"
 #include "chre/util/system/debug_dump.h"
-#include "chre_api/chre/version.h"
 
 #include <algorithm>
 
@@ -128,23 +127,19 @@ void Nanoapp::cycleWakeupBuckets(size_t numBuckets) {
 }
 
 void Nanoapp::logStateToBuffer(DebugDumpWrapper &debugDump) const {
-  debugDump.print(" Id=%" PRIu32 " 0x%016" PRIx64 " ", getInstanceId(),
-                  getAppId());
   PlatformNanoapp::logStateToBuffer(debugDump);
-  debugDump.print(" v%" PRIu32 ".%" PRIu32 ".%" PRIu32 " tgtAPI=%" PRIu32
-                  ".%" PRIu32 " curAlloc=%zu peakAlloc=%zu",
-                  CHRE_EXTRACT_MAJOR_VERSION(getAppVersion()),
-                  CHRE_EXTRACT_MINOR_VERSION(getAppVersion()),
-                  CHRE_EXTRACT_PATCH_VERSION(getAppVersion()),
-                  CHRE_EXTRACT_MAJOR_VERSION(getTargetApiVersion()),
-                  CHRE_EXTRACT_MINOR_VERSION(getTargetApiVersion()),
-                  getTotalAllocatedBytes(), getPeakAllocatedBytes());
-  debugDump.print(" hostWakeups=[ cur->");
+  debugDump.print(" Id=%" PRIu32 " AppId=0x%016" PRIx64 " ver=0x%" PRIx32
+                  " targetAPI=0x%" PRIx32
+                  " currentAllocatedBytes=%zu peakAllocatedBytes=%zu",
+                  getInstanceId(), getAppId(), getAppVersion(),
+                  getTargetApiVersion(), getTotalAllocatedBytes(),
+                  getPeakAllocatedBytes());
+  debugDump.print(" HostWakeups=[ Latest-> ");
   // Get buckets latest -> earliest except last one
   for (size_t i = mWakeupBuckets.size() - 1; i > 0; --i) {
     debugDump.print("%" PRIu16 ", ", mWakeupBuckets[i]);
   }
-  // Earliest bucket gets no comma
+  // earliest bucket gets no comma
   debugDump.print("%" PRIu16 " ]\n", mWakeupBuckets.front());
 }
 
