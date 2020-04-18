ceef55d71b9cd4b7754a6a4e94c482c67920f9c8
openvela-robot <openvela-robot@xiaomi.com>
2020-04-18 23:10:01 +0800

    Snap for 6408188 from 4a92149476abf8116d7e332fb44484efcb4698a5 to rvc-release
    Change-Id: I8753ecd58b6550b1259cb2ac51f57d96c6a8ba75

--- chpp/include/chpp/macros.h
+++ chpp/include/chpp/macros.h
@@ -17,11 +17,7 @@
 #ifndef CHPP_MACROS_H_
 #define CHPP_MACROS_H_
 
-#include <assert.h>
-#include <stddef.h>
 #include <stdlib.h>
-
-#include <bits/types.h>
 
 #ifdef __cplusplus
 extern "C" {
@@ -46,18 +42,6 @@ extern "C" {
 #ifndef CHPP_NOT_NULL
 #define CHPP_NOT_NULL(var) CHPP_ASSERT((var) != NULL)
 #endif
-
-#if defined(__GNUC__) || defined(__clang__)
-#define check_types_match(t1, t2) ((typeof(t1) *)0 != (typeof(t2) *)0)
-#else
-#define check_types_match(t1, t2) 0
-#endif
-
-#define container_of(ptr, type, member)             \
-  ((type *)((char *)(ptr)-offsetof(type, member)) + \
-   check_types_match(*(ptr), ((type *)0)->member))
-
-#define sizeof_member(type, member) (sizeof(((type *)0)->member))
 
 /**
  * Macros for defining (compiler dependent) packed structures
