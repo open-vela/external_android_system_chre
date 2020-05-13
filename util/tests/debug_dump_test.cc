5f135fba06de038dcf6b4b34c703029b53a05065
openvela-robot <openvela-robot@xiaomi.com>
2020-05-13 16:45:58 +0800

    Move service open/close trigger to the client side
    Service open/close triggering to the client side instead of at service
    registration.
    
    Bug: 146020579
    Test: ran "atest --host chre_chpp_linux_tests -c"
    Change-Id: I72f52f4f77189e38f559ebc5c6be71e7894bd7fb

--- util/tests/debug_dump_test.cc
+++ util/tests/debug_dump_test.cc
@@ -58,15 +58,7 @@ TEST(DebugDumpWrapper, TooLargeOfStringToFit) {
   const char *str = "a";
   debugDump.print("%s", str);
   const auto &buffers = debugDump.getBuffers();
-
-  // One null-terminated buffer will be created for an empty wrapper.
-  EXPECT_EQ(buffers.size(), 1);
-  EXPECT_TRUE(strcmp(buffers.back().get(), "") == 0);
-
-  // Once there's a buffer, it won't be updated.
-  debugDump.print("%s", str);
-  EXPECT_EQ(buffers.size(), 1);
-  EXPECT_TRUE(strcmp(buffers.back().get(), "") == 0);
+  EXPECT_TRUE(buffers.empty());
 }
 
 TEST(DebugDumpWrapper, TooLargeOfStringWithPartlyFilledBuffer) {
