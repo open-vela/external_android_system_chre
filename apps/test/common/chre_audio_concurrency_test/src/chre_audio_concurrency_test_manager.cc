0c5f6dd1733b830d062d3a6952927cf135ed9349
openvela-robot <openvela-robot@xiaomi.com>
2020-05-17 22:06:27 +0800

    Audio concurrency test: enable audio/verify received am: b8153bdd5a am: a68f2df6d3
    Change-Id: I720a44294e6b28cb2ab3594dd0fa18475e822e26

--- apps/test/common/chre_audio_concurrency_test/src/chre_audio_concurrency_test_manager.cc
+++ apps/test/common/chre_audio_concurrency_test/src/chre_audio_concurrency_test_manager.cc
@@ -68,6 +68,7 @@ Manager::~Manager() {
     chreAudioConfigureSource(kAudioHandle, false /* enable */,
                              0 /* bufferDuration */, 0 /* deliveryInterval */);
   }
+  cancelTimeoutTimer();
 }
 
 bool Manager::handleTestCommandMessage(uint16_t hostEndpointId, TestStep step) {
@@ -90,16 +91,10 @@ bool Manager::handleTestCommandMessage(uint16_t hostEndpointId, TestStep step) {
         // Start a timer to ensure we receive the first audio data event
         // quickly. Since it may take some time to load the sound model, choose
         // a reasonably long timeout.
-        mTimerHandle = chreTimerSet(10 * kOneSecondInNanoseconds,
-                                    nullptr /* cookie */, true /* oneShot */);
-        if (mTimerHandle == CHRE_TIMER_INVALID) {
-          LOGE("Failed to set audio enabled timer");
-        } else {
-          success = true;
-        }
+        success = setTimeoutTimer(10 /* durationSeconds */);
       }
     } else if (step == TestStep::VERIFY_AUDIO_RESUME) {
-      // TODO: Verify audio resumes
+      success = setTimeoutTimer(10 /* durationSeconds */);
     }
 
     if (success) {
@@ -162,17 +157,36 @@ void Manager::handleDataFromChre(uint16_t eventType, const void *eventData) {
 }
 
 void Manager::handleTimer() {
-  // TODO: Timeout failure
+  if (mTimerHandle != CHRE_TIMER_INVALID && mTestSession.has_value()) {
+    LOGE("Timed out during test: step %" PRIu8, mTestSession->step);
+    sendTestResultToHost(mTestSession->hostEndpointId, kTestResultMessageType,
+                         false /* success */);
+    mTestSession.reset();
+  }
+}
+
+bool Manager::setTimeoutTimer(size_t durationSeconds) {
+  mTimerHandle = chreTimerSet(durationSeconds * kOneSecondInNanoseconds,
+                              nullptr /* cookie */, true /* oneShot */);
+  if (mTimerHandle == CHRE_TIMER_INVALID) {
+    LOGE("Failed to set timeout timer");
+  }
+
+  return mTimerHandle != CHRE_TIMER_INVALID;
+}
+
+void Manager::cancelTimeoutTimer() {
+  if (mTimerHandle != CHRE_TIMER_INVALID) {
+    chreTimerCancel(mTimerHandle);
+    mTimerHandle = CHRE_TIMER_INVALID;
+  }
 }
 
 void Manager::handleAudioDataEvent(const chreAudioDataEvent *data) {
   if (mTestSession.has_value()) {
     switch (mTestSession->step) {
       case TestStep::ENABLE_AUDIO: {
-        if (mTimerHandle != CHRE_TIMER_INVALID) {
-          chreTimerCancel(mTimerHandle);
-          mTimerHandle = CHRE_TIMER_INVALID;
-        }
+        cancelTimeoutTimer();
         sendEmptyMessageToHost(
             mTestSession->hostEndpointId,
             chre_audio_concurrency_test_MessageType_TEST_AUDIO_ENABLED);
@@ -180,11 +194,17 @@ void Manager::handleAudioDataEvent(const chreAudioDataEvent *data) {
         // Reset the test session to avoid sending multiple TEST_AUDIO_ENABLED
         // messages to the host, while we wait for the next step.
         mTestSession.reset();
+
+        // TODO: Perform sanity check on audio data
         break;
       }
 
       case TestStep::VERIFY_AUDIO_RESUME: {
-        // TODO:
+        cancelTimeoutTimer();
+        sendTestResultToHost(mTestSession->hostEndpointId,
+                             kTestResultMessageType, true /* success */);
+        mTestSession.reset();
+        break;
       }
 
       default:
