0547df8b4b2755c34024fabe79f7e4223fbf9649
openvela-robot <openvela-robot@xiaomi.com>
2020-06-05 15:07:43 +0800

    [automerger skipped] Modify makefile to support running PAL tests am: 88a236f7ba am: 20d2d9b322 am: 41ff2ef48f -s ours
    am skip reason: Change-Id I45274f9467fd4ffde2dceac213358cd386d796f9 with SHA-1 6dc849e1d4 is in history
    
    Original change: https://googleplex-android-review.googlesource.com/c/platform/system/chre/+/11744085
    
    Change-Id: Iadc65ea08aa5f27f44d6b3fae2429be01b7a1eac

--- pal/pal.mk
+++ pal/pal.mk
@@ -10,4 +10,5 @@ COMMON_CFLAGS += -Ipal/include
 # GoogleTest Source Files ######################################################
 
 GOOGLETEST_SRCS += pal/tests/version_test.cc
+GOOGLETEST_SRCS += pal/tests/wwan_test.cc
 GOOGLETEST_PAL_IMPL_SRCS += pal/tests/wifi_pal_impl_test.cc
