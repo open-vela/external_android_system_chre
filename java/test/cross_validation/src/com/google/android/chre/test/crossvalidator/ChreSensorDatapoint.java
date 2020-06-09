f9d69681031c6fb1e246b22c972aff56e83b9bbb
openvela-robot <openvela-robot@xiaomi.com>
2020-06-09 17:40:24 +0800

    Merge "CHPP_FREE_AND_NULLIFY() ensures freed memory pointers are set to null"

--- java/test/cross_validation/src/com/google/android/chre/test/crossvalidator/ChreSensorDatapoint.java
+++ java/test/cross_validation/src/com/google/android/chre/test/crossvalidator/ChreSensorDatapoint.java
@@ -15,7 +15,7 @@
  */
 package com.google.android.chre.test.crossvalidator;
 
-import com.google.android.chre.nanoapp.proto.ChreCrossValidationSensor;
+import com.google.android.chre.nanoapp.proto.ChreCrossValidation;
 import com.google.common.primitives.Floats;
 
 /*package*/
@@ -29,7 +29,7 @@ class ChreSensorDatapoint extends SensorDatapoint {
     * @param datapoint The chre cross validation proto message for datapoint from CHRE.
     */
     /*package*/
-    ChreSensorDatapoint(ChreCrossValidationSensor.SensorDatapoint datapoint) {
+    ChreSensorDatapoint(ChreCrossValidation.SensorDatapoint datapoint) {
         timestamp = datapoint.getTimestampInNs();
         values = Floats.toArray(datapoint.getValuesList());
     }
