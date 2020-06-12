cb900e214dfc30ca349fd14af65eedbd0a9a1682
openvela-robot <openvela-robot@xiaomi.com>
2020-06-13 00:49:26 +0800

    Move globals vars to test fixture am: 3815993bb2
    Original change: https://android-review.googlesource.com/c/platform/system/chre/+/1330885
    
    Change-Id: Iecd641d737c3f768a886585b60aba0cdb3171262

--- pal/pal.mk
+++ pal/pal.mk
@@ -11,5 +11,4 @@ COMMON_CFLAGS += -Ipal/include
 
 GOOGLETEST_CFLAGS += -Ipal/tests/include
 GOOGLETEST_SRCS += pal/tests/src/version_test.cc
-GOOGLETEST_SRCS += pal/tests/src/wwan_test.cc
 GOOGLETEST_PAL_IMPL_SRCS += pal/tests/src/wifi_pal_impl_test.cc
