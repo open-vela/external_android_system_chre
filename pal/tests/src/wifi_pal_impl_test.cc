78fbd9dc61c58f13f3cb578cb0b8fbdf70c75feb
liujinye <liujinye@xiaomi.com>
2020-06-12 20:03:30 +0800

    Merge "Add test to verify scan monitor"

--- pal/tests/src/wifi_pal_impl_test.cc
+++ pal/tests/src/wifi_pal_impl_test.cc
@@ -21,9 +21,19 @@
 #include "chre/platform/system_time.h"
 #include "chre/util/lock_guard.h"
 #include "chre/util/nanoapp/wifi.h"
-#include "chre/util/time.h"
 
 #include <cinttypes>
+
+// Flag to require on-demand WiFi scanning to be enabled for the test to pass.
+// Set to false to allow tests to pass on disabled platforms.
+#ifndef PAL_IMPL_TEST_WIFI_ON_DEMAND_SCAN_REQUIRED
+#define PAL_IMPL_TEST_WIFI_ON_DEMAND_SCAN_REQUIRED true
+#endif
+
+// Same as above for scan monitoring.
+#ifndef PAL_IMPL_TEST_WIFI_SCAN_MONITORING_REQUIRED
+#define PAL_IMPL_TEST_WIFI_SCAN_MONITORING_REQUIRED true
+#endif
 
 namespace {
 
@@ -33,6 +43,12 @@ using ::chre::SystemTime;
 
 //! A pointer to the current test running
 wifi_pal_impl_test::PalWifiTest *gTest = nullptr;
+
+//! Timeout as specified by the CHRE API
+const Nanoseconds kAsyncResultTimeoutNs =
+    Nanoseconds(CHRE_ASYNC_RESULT_TIMEOUT_NS);
+const Nanoseconds kScanResultTimeoutNs =
+    Nanoseconds(CHRE_WIFI_SCAN_RESULT_TIMEOUT_NS);
 
 void chrePalScanMonitorStatusChangeCallback(bool enabled, uint8_t errorCode) {
   if (gTest != nullptr) {
@@ -113,6 +129,9 @@ void PalWifiTest::SetUp() {
   numScanResultCount_ = 0;
   lastScanEventReceived_ = false;
   scanEventList_.clear();
+  scanParams_.reset();
+  lastEventIndex_ = UINT8_MAX;
+  scanMonitorEnabled_ = false;
 }
 
 void PalWifiTest::TearDown() {
@@ -122,7 +141,12 @@ void PalWifiTest::TearDown() {
 
 void PalWifiTest::scanMonitorStatusChangeCallback(bool enabled,
                                                   uint8_t errorCode) {
-  // TODO:
+  LOGI("Received scan monitor response with enabled %d error %" PRIu8, enabled,
+       errorCode);
+  chre::LockGuard<chre::Mutex> lock(mutex_);
+  scanMonitorEnabled_ = enabled;
+  errorCode_ = errorCode;
+  condVar_.notify_one();
 }
 
 void PalWifiTest::scanResponseCallback(bool pending, uint8_t errorCode) {
@@ -153,7 +177,39 @@ void PalWifiTest::rangingEventCallback(uint8_t errorCode,
   // TODO:
 }
 
+void PalWifiTest::validateWifiScanEvent(const chreWifiScanEvent &event) {
+  if (scanParams_.has_value()) {
+    EXPECT_EQ(event.scanType, scanParams_->scanType);
+    EXPECT_GE(event.referenceTime,
+              chreGetTime() - (scanParams_->maxScanAgeMs *
+                               chre::kOneMillisecondInNanoseconds));
+    EXPECT_EQ(event.radioChainPref, scanParams_->radioChainPref);
+    EXPECT_EQ(event.eventIndex, static_cast<uint8_t>(lastEventIndex_ + 1));
+  }
+}
+
+void PalWifiTest::waitForAsyncResponseAssertSuccess(
+    chre::Nanoseconds timeoutNs) {
+  Nanoseconds end = SystemTime::getMonotonicTime() + timeoutNs;
+  while (errorCode_ == CHRE_ERROR_LAST &&
+         SystemTime::getMonotonicTime() < end) {
+    condVar_.wait_for(mutex_, timeoutNs);
+  }
+  ASSERT_LT(SystemTime::getMonotonicTime(), end);
+  ASSERT_EQ(errorCode_, CHRE_ERROR_NONE);
+}
+
 TEST_F(PalWifiTest, ScanAsyncTest) {
+#if PAL_IMPL_TEST_WIFI_ON_DEMAND_SCAN_REQUIRED
+  ASSERT_EQ(api_->getCapabilities() & CHRE_WIFI_CAPABILITIES_ON_DEMAND_SCAN,
+            CHRE_WIFI_CAPABILITIES_ON_DEMAND_SCAN);
+#else
+  if ((api_->getCapabilities() & CHRE_WIFI_CAPABILITIES_ON_DEMAND_SCAN) !=
+      CHRE_WIFI_CAPABILITIES_ON_DEMAND_SCAN) {
+    GTEST_SKIP();
+  }
+#endif
+
   // Request a WiFi scan
   chre::LockGuard<chre::Mutex> lock(mutex_);
 
@@ -163,36 +219,58 @@ TEST_F(PalWifiTest, ScanAsyncTest) {
   params.frequencyListLen = 0;
   params.ssidListLen = 0;
   params.radioChainPref = CHRE_WIFI_RADIO_CHAIN_PREF_DEFAULT;
-  ASSERT_TRUE(api_->requestScan(&params));
+  scanParams_ = params;
 
-  const Nanoseconds kTimeoutNs = Nanoseconds(CHRE_WIFI_SCAN_RESULT_TIMEOUT_NS);
-  Nanoseconds end = SystemTime::getMonotonicTime() + kTimeoutNs;
-  errorCode_ = CHRE_ERROR_LAST;
-  while (errorCode_ == CHRE_ERROR_LAST &&
-         SystemTime::getMonotonicTime() < end) {
-    condVar_.wait_for(mutex_, kTimeoutNs);
-  }
-  ASSERT_LT(SystemTime::getMonotonicTime(), end);
-  ASSERT_EQ(errorCode_, CHRE_ERROR_NONE);
+  prepareForAsyncResponse();
+  ASSERT_TRUE(api_->requestScan(&scanParams_.value()));
+  waitForAsyncResponseAssertSuccess(kScanResultTimeoutNs);
 
   // The CHRE API only poses timeout requirements on the async response. Use
   // the same timeout to receive the scan results to avoid blocking forever.
-  end = SystemTime::getMonotonicTime() + kTimeoutNs;
+  Nanoseconds end = SystemTime::getMonotonicTime() + kScanResultTimeoutNs;
   while (!lastScanEventReceived_ && SystemTime::getMonotonicTime() < end) {
-    condVar_.wait_for(mutex_, kTimeoutNs);
+    condVar_.wait_for(mutex_, kScanResultTimeoutNs);
   }
 
   for (auto *event : scanEventList_) {
-    // TODO: Sanity check values
     for (uint8_t i = 0; i < event->resultCount; i++) {
       const chreWifiScanResult &result = event->results[i];
       logChreWifiResult(result);
     }
+    validateWifiScanEvent(*event);
+
+    lastEventIndex_ = event->eventIndex;
     api_->releaseScanEvent(event);
   }
 
   EXPECT_TRUE(lastScanEventReceived_);
   EXPECT_GT(numScanResultCount_, 0);
+}
+
+// Note: This test only verifies that the scan monitor succeeds according
+// to the async response.
+TEST_F(PalWifiTest, ScanMonitorTest) {
+#if PAL_IMPL_TEST_WIFI_SCAN_MONITORING_REQUIRED
+  ASSERT_EQ(api_->getCapabilities() & CHRE_WIFI_CAPABILITIES_SCAN_MONITORING,
+            CHRE_WIFI_CAPABILITIES_SCAN_MONITORING);
+#else
+  if ((api_->getCapabilities() & CHRE_WIFI_CAPABILITIES_SCAN_MONITORING) !=
+      CHRE_WIFI_CAPABILITIES_SCAN_MONITORING) {
+    GTEST_SKIP();
+  }
+#endif
+
+  chre::LockGuard<chre::Mutex> lock(mutex_);
+
+  prepareForAsyncResponse();
+  ASSERT_TRUE(api_->configureScanMonitor(true /* enable */));
+  waitForAsyncResponseAssertSuccess(kAsyncResultTimeoutNs);
+  ASSERT_TRUE(scanMonitorEnabled_);
+
+  prepareForAsyncResponse();
+  ASSERT_TRUE(api_->configureScanMonitor(false /* enable */));
+  waitForAsyncResponseAssertSuccess(kAsyncResultTimeoutNs);
+  ASSERT_FALSE(scanMonitorEnabled_);
 }
 
 }  // namespace wifi_pal_impl_test
