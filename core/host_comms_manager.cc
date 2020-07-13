de27310b16e73240645e7ab4d403212bf238e6c6
liujinye <liujinye@xiaomi.com>
2020-07-13 10:29:46 +0800

    Regenerate QAIC files with Hexagon SDK v3.5.2
    Also update the script to point to the modified path.
    
    Bug: 163418866
    Test: confirm CHRE initializes and chre_test_client functions normally
          when only daemon is updated, and with recompiled
          libchre_slpi_skel.so
    Merged-In: Ieb28f896a83ba2c952d5075c9adb5056fce2efbe
    Change-Id: Ieb28f896a83ba2c952d5075c9adb5056fce2efbe
    (cherry picked from commit a7971bcfbd29d5735650372570098540701828b5)

--- core/host_comms_manager.cc
+++ core/host_comms_manager.cc
@@ -26,6 +26,15 @@ namespace chre {
 
 constexpr uint32_t kMessageToHostReservedFieldValue = UINT32_MAX;
 
+void HostCommsManager::flushMessagesSentByNanoapp(uint64_t appId) {
+  mHostLink.flushMessagesSentByNanoapp(appId);
+}
+
+void HostCommsManager::sendLogMessage(const char *logMessage,
+                                      size_t logMessageSize) {
+  mHostLink.sendLogMessage(logMessage, logMessageSize);
+}
+
 bool HostCommsManager::sendMessageToHostFromNanoapp(
     Nanoapp *nanoapp, void *messageData, size_t messageSize,
     uint32_t messageType, uint16_t hostEndpoint,
@@ -60,7 +69,7 @@ bool HostCommsManager::sendMessageToHostFromNanoapp(
                               .getPowerControlManager()
                               .hostIsAwake();
 
-      success = HostLink::sendMessage(msgToHost);
+      success = mHostLink.sendMessage(msgToHost);
       if (!success) {
         mMessagePool.deallocate(msgToHost);
       } else if (!hostWasAwake && !mIsNanoappBlamedForWakeup) {
