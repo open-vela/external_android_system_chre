9bd0ce49b01a87c9a8bdde252d9d40e0db0539e6
openvela-robot <openvela-robot@xiaomi.com>
2020-07-15 20:31:19 +0800

    Revamp documentation am: 7f23a87057
    Original change: https://googleplex-android-review.googlesource.com/c/platform/system/chre/+/12063633
    
    Change-Id: I6ff56c151be2170c2cd9a9927fa596dbe94d232a

--- build/arch/x86.mk
+++ build/arch/x86.mk
@@ -28,9 +28,6 @@ TARGET_CFLAGS += -g
 # Enable position independence.
 TARGET_CFLAGS += -fpic
 
-# Disable double promotion warning for logging
-TARGET_CFLAGS += -Wno-double-promotion
-
 # x86 Shared Object Linker Flags ###############################################
 
 TARGET_SO_LDFLAGS += -shared
