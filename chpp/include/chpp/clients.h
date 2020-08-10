4bdba3768fb36d497194aa6f4d8c224482c36c58
openvela-robot <openvela-robot@xiaomi.com>
2020-08-11 01:08:56 +0800

    Merge "Ignore cal sensor discovery failure if disabled"

--- chpp/include/chpp/clients.h
+++ chpp/include/chpp/clients.h
@@ -88,9 +88,6 @@ struct ChppClientState {
 #define CHPP_CLIENT_ENABLED_GNSS
 #endif
 
-// The default timeout for chppSendTimestampedRequestAndWait().
-#define DEFAULT_CLIENT_REQUEST_TIMEOUT_NS UINT64_C(5000000000)  // 5s
-
 /************************************************
  *  Public functions
  ***********************************************/
@@ -252,20 +249,11 @@ bool chppSendTimestampedRequestOrFail(struct ChppClientState *clientState,
  * @param len Datagram length in bytes.
  *
  * @return True informs the sender that the datagram was successfully enqueued.
- * False informs the sender that the payload was discarded because either the
- * queue was full, or the request timed out.
+ * False informs the sender that the queue was full and the payload discarded.
  */
 bool chppSendTimestampedRequestAndWait(struct ChppClientState *clientState,
                                        struct ChppRequestResponseState *rRState,
                                        void *buf, size_t len);
-
-/**
- * Same as chppSendTimestampedRequestAndWait() but with a specified timeout.
- */
-bool chppSendTimestampedRequestAndWaitTimeout(
-    struct ChppClientState *clientState,
-    struct ChppRequestResponseState *rRState, void *buf, size_t len,
-    uint64_t timeoutNs);
 
 #ifdef __cplusplus
 }
