bc7c5b3b8f4d03ba59df1ae0b5bb9be0232e349a
openvela-robot <openvela-robot@xiaomi.com>
2020-08-28 02:37:52 +0800

    Merge "Log Error for Sync Request Response Timeout"

--- platform/linux/pal_gnss.cc
+++ platform/linux/pal_gnss.cc
@@ -20,7 +20,6 @@
 #include "chre/util/unique_ptr.h"
 
 #include <chrono>
-#include <cinttypes>
 #include <future>
 #include <thread>
 
@@ -165,7 +164,8 @@ bool chrePalGnssApiOpen(const struct chrePalSystemApi *systemApi,
 
 }  // anonymous namespace
 
-const struct chrePalGnssApi *chrePalGnssGetApi(uint32_t requestedApiVersion) {
+const struct chrePalGnssApi *chrePalGnssGetApi(
+    uint32_t /* requestedApiVersion */) {
   static const struct chrePalGnssApi kApi = {
       .moduleVersion = CHRE_PAL_GNSS_API_CURRENT_VERSION,
       .open = chrePalGnssApiOpen,
@@ -177,13 +177,5 @@ const struct chrePalGnssApi *chrePalGnssGetApi(uint32_t requestedApiVersion) {
       .releaseMeasurementDataEvent = chrePalGnssReleaseMeasurementDataEvent,
   };
 
-  if (!CHRE_PAL_VERSIONS_ARE_COMPATIBLE(kApi.moduleVersion,
-                                        requestedApiVersion)) {
-    LOGE("Incompatible version: requested 0x%" PRIx32
-         " module version 0x%" PRIx32,
-         requestedApiVersion, kApi.moduleVersion);
-    return nullptr;
-  } else {
-    return &kApi;
-  }
+  return &kApi;
 }
