96633e2c57b1222e1a0ca389afdc72a4483e69b6
liujinye <liujinye@xiaomi.com>
2020-08-28 02:37:52 +0800

    Merge "Log Error for Sync Request Response Timeout"

--- chpp/test/loopback_test.cpp
+++ chpp/test/loopback_test.cpp
@@ -16,29 +16,98 @@
 
 #include <gtest/gtest.h>
 
+#include <stdbool.h>
 #include <stddef.h>
 #include <stdint.h>
 #include <string.h>
 #include <thread>
 
-#include "app_test_base.h"
 #include "chpp/app.h"
 #include "chpp/clients/loopback.h"
+#include "chpp/macros.h"
 #include "chpp/platform/log.h"
+#include "chpp/transport.h"
+
+namespace {
+
+void *workThread(void *arg) {
+  ChppTransportState *context = static_cast<ChppTransportState *>(arg);
+  pthread_setname_np(pthread_self(), context->linkParams.workThreadName);
+
+  chppWorkThreadStart(context);
+
+  return nullptr;
+}
 
 /*
  * Test suite for the CHPP Loopback client/service
  */
-namespace chpp {
-namespace {
+class LoopbackTests : public testing::Test {
+ protected:
+  void SetUp() override {
+    memset(&mClientTransportContext.linkParams, 0,
+           sizeof(mClientTransportContext.linkParams));
+    memset(&mServiceTransportContext.linkParams, 0,
+           sizeof(mServiceTransportContext.linkParams));
+    // The linkSendThread in the link layer is a link "to" the remote end.
+    mServiceTransportContext.linkParams.linkThreadName = "Link to client";
+    mServiceTransportContext.linkParams.workThreadName = "Service work";
+    mClientTransportContext.linkParams.linkThreadName = "Link to service";
+    mClientTransportContext.linkParams.workThreadName = "Client work";
 
-TEST_F(AppTestBase, SimpleStartStop) {
+    chppTransportInit(&mClientTransportContext, &mClientAppContext);
+    chppAppInit(&mClientAppContext, &mClientTransportContext);
+
+    chppTransportInit(&mServiceTransportContext, &mServiceAppContext);
+    chppAppInit(&mServiceAppContext, &mServiceTransportContext);
+
+    mClientTransportContext.linkParams.remoteTransportContext =
+        &mServiceTransportContext;
+    mServiceTransportContext.linkParams.remoteTransportContext =
+        &mClientTransportContext;
+
+    pthread_create(&mClientWorkThread, NULL, workThread,
+                   &mClientTransportContext);
+
+    // Wait a bit to emulate the scenario where the remote is not yet up
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+    pthread_create(&mServiceWorkThread, NULL, workThread,
+                   &mServiceTransportContext);
+    mClientTransportContext.linkParams.linkEstablished = true;
+    mServiceTransportContext.linkParams.linkEstablished = true;
+  }
+
+  void TearDown() override {
+    chppWorkThreadStop(&mClientTransportContext);
+    pthread_join(mClientWorkThread, NULL);
+
+    chppAppDeinit(&mClientAppContext);
+    chppTransportDeinit(&mClientTransportContext);
+
+    chppWorkThreadStop(&mServiceTransportContext);
+    pthread_join(mServiceWorkThread, NULL);
+
+    chppAppDeinit(&mServiceAppContext);
+    chppTransportDeinit(&mServiceTransportContext);
+  }
+
+  ChppTransportState mClientTransportContext = {};
+  ChppAppState mClientAppContext = {};
+
+  ChppTransportState mServiceTransportContext = {};
+  ChppAppState mServiceAppContext = {};
+
+  pthread_t mClientWorkThread;
+  pthread_t mServiceWorkThread;
+};
+
+TEST_F(LoopbackTests, SimpleStartStop) {
   // Simple test to make sure start/stop work threads work,
   // without crashes.
   std::this_thread::sleep_for(std::chrono::seconds(1));
 }
 
-TEST_F(AppTestBase, SimpleLoopback) {
+TEST_F(LoopbackTests, SimpleLoopback) {
   // Wait for the reset to finish.
   std::this_thread::sleep_for(std::chrono::seconds(1));
 
@@ -65,5 +134,4 @@ TEST_F(AppTestBase, SimpleLoopback) {
   EXPECT_EQ(result.error, CHPP_APP_ERROR_INVALID_LENGTH);
 }
 
-}  //  anonymous namespace
-}  //  namespace chpp
+}  // namespace
