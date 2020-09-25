9a934fbd0d26d1b51a935f5ed2a312aec61f4606
openvela-robot <openvela-robot@xiaomi.com>
2020-09-25 18:35:03 +0800

    [automerger skipped] Stop SeeCalHelper from autoregistration during init am: 02e813fd31 -s ours
    am skip reason: Change-Id I2be4a9124a801bddf2c8a1d39bf526b51ba8ec17 with SHA-1 79b1a080f9 is in history
    
    Original change: https://android-review.googlesource.com/c/platform/system/chre/+/1241966
    
    Change-Id: Ia493f55c7592e01e643a1602be87520c79fbc211

--- platform/slpi/see/see_helper.cc
+++ platform/slpi/see/see_helper.cc
@@ -1707,21 +1707,11 @@ bool SeeHelper::init(SeeHelperCallbackInterface *cbIf, Microseconds timeout,
   sns_client *client;
 
   // Initialize cal/remote_proc_state sensors before making sensor data request.
-  bool success = waitForService(&client, timeout) &&
-                 mSeeClients.push_back(client) && initResamplerSensor();
-  if (success && !skipDefaultSensorInit) {
-    if (!mCalHelper->findCalibrationSensors(*this)) {
-#ifdef CHRE_LOG_ONLY_NO_CAL_SENSOR
-      LOGW("Bypassing failure to find calibrated sensor");
-#else   // CHRE_LOG_ONLY_NO_CAL_SENSOR
-      success = false;
-#endif  // CHRE_LOG_ONLY_NO_CAL_SENSOR
-    }
-    if (success) {
-      success = initRemoteProcSensor();
-    }
-  }
-  return success;
+  return (waitForService(&client, timeout) && mSeeClients.push_back(client) &&
+          initResamplerSensor() &&
+          (skipDefaultSensorInit ||
+           (mCalHelper->registerForCalibrationUpdates(*this) &&
+            initRemoteProcSensor())));
 }
 
 bool SeeHelper::makeRequest(const SeeSensorRequest &request) {
