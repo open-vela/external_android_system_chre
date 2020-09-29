bd17b36a8ed33bd4935ec2fc3d4629e9b99246c6
liujinye <liujinye@xiaomi.com>
2020-09-30 05:15:37 +0800

    Merge updated CHQTS code to rvc-dev am: 2aeb18ef8e am: 84baae589e
    Original change: https://googleplex-android-review.googlesource.com/c/platform/system/chre/+/12697522
    
    Change-Id: I61f2f4e1afd627fa5b6723e3d58296fab8b9221b

--- core/wifi_request_manager.cc
+++ core/wifi_request_manager.cc
@@ -407,10 +407,14 @@ bool WifiRequestManager::postScanMonitorAsyncResultEvent(
       event->reserved = 0;
       event->cookie = cookie;
 
-      EventLoopManagerSingleton::get()->getEventLoop().postEventOrDie(
-          CHRE_EVENT_WIFI_ASYNC_RESULT, event, freeEventDataCallback,
-          nanoappInstanceId);
-      eventPosted = true;
+      // Post the event.
+      eventPosted =
+          EventLoopManagerSingleton::get()->getEventLoop().postEventOrDie(
+              CHRE_EVENT_WIFI_ASYNC_RESULT, event, freeEventDataCallback,
+              nanoappInstanceId);
+      if (!eventPosted) {
+        memoryFree(event);
+      }
     }
   }
 
@@ -429,9 +433,6 @@ void WifiRequestManager::postScanMonitorAsyncResultEventFatal(
 bool WifiRequestManager::postScanRequestAsyncResultEvent(
     uint32_t nanoappInstanceId, bool success, uint8_t errorCode,
     const void *cookie) {
-  // TODO: the body of this function can be extracted to a common helper for use
-  // across this function, postScanMonitorAsyncResultEvent,
-  // postRangingAsyncResult, and GnssSession::postAsyncResultEvent
   bool eventPosted = false;
   chreAsyncResult *event = memoryAlloc<chreAsyncResult>();
   if (event == nullptr) {
@@ -443,10 +444,11 @@ bool WifiRequestManager::postScanRequestAsyncResultEvent(
     event->reserved = 0;
     event->cookie = cookie;
 
-    EventLoopManagerSingleton::get()->getEventLoop().postEventOrDie(
-        CHRE_EVENT_WIFI_ASYNC_RESULT, event, freeEventDataCallback,
-        nanoappInstanceId);
-    eventPosted = true;
+    // Post the event.
+    eventPosted =
+        EventLoopManagerSingleton::get()->getEventLoop().postEventOrDie(
+            CHRE_EVENT_WIFI_ASYNC_RESULT, event, freeEventDataCallback,
+            nanoappInstanceId);
   }
 
   return eventPosted;
@@ -587,10 +589,13 @@ bool WifiRequestManager::postRangingAsyncResult(uint8_t errorCode) {
       event->reserved = 0;
       event->cookie = req.cookie;
 
-      EventLoopManagerSingleton::get()->getEventLoop().postEventOrDie(
-          CHRE_EVENT_WIFI_ASYNC_RESULT, event, freeEventDataCallback,
-          req.nanoappInstanceId);
-      eventPosted = true;
+      eventPosted =
+          EventLoopManagerSingleton::get()->getEventLoop().postEventOrDie(
+              CHRE_EVENT_WIFI_ASYNC_RESULT, event, freeEventDataCallback,
+              req.nanoappInstanceId);
+      if (!eventPosted) {
+        memoryFree(event);
+      }
     }
   }
 
