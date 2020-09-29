0c823fd346a691cee4012a4b4309659728dd583b
openvela-robot <openvela-robot@xiaomi.com>
2020-09-29 21:43:25 +0800

    Merge updated CHQTS code to rvc-dev am: 2aeb18ef8e
    Original change: https://googleplex-android-review.googlesource.com/c/platform/system/chre/+/12697522
    
    Change-Id: Ibd688b7f56083bbfe27862cba9101f90b2e35254

--- chpp/include/chpp/macros.h
+++ chpp/include/chpp/macros.h
@@ -17,28 +17,22 @@
 #ifndef CHPP_MACROS_H_
 #define CHPP_MACROS_H_
 
-#include <assert.h>
-#include <inttypes.h>
-#include <stddef.h>
 #include <stdlib.h>
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-#define MOST_SIGNIFICANT_NIBBLE 0xf0
-#define LEAST_SIGNIFICANT_NIBBLE 0x0f
-
 #ifndef UNUSED_VAR
 #define UNUSED_VAR(var) ((void)(var))
 #endif
 
 #ifndef MIN
-#define MIN(a, b) ((a) < (b) ? (a) : (b))
+#define MIN(a, b) ((a) < (b)) ? (a) : (b)
 #endif
 
 #ifndef MAX
-#define MAX(a, b) ((a) > (b) ? (a) : (b))
+#define MAX(a, b) ((a) > (b)) ? (a) : (b)
 #endif
 
 #ifndef CHPP_ASSERT
@@ -49,52 +43,6 @@ extern "C" {
 #define CHPP_NOT_NULL(var) CHPP_ASSERT((var) != NULL)
 #endif
 
-#ifndef CHPP_DEBUG_ASSERT
-#define CHPP_DEBUG_ASSERT(var) CHPP_ASSERT(var)
-#endif
-
-#ifndef PRIu64  // Pre-C99 lacks PRIu64 / llu support
-#define PRIu64 "lu"
-#endif
-
-#if defined(__GNUC__) && (__STDC_VERSION__ >= 201112L)
-#define CHPP_C11_OR_NEWER
-#endif
-
-#ifdef CHPP_C11_OR_NEWER
-#define CHPP_STATIC_ASSERT _Static_assert
-
-#else
-#define CHPP_STATIC_ASSERT4(cond, var) typedef char var[(!!(cond)) * 2 - 1]
-#define CHPP_STATIC_ASSERT3(cond, line) \
-  CHPP_STATIC_ASSERT4(cond, static_assertion_at_line_##line)
-#define CHPP_STATIC_ASSERT2(cond, line) CHPP_STATIC_ASSERT3(cond, line)
-#define CHPP_STATIC_ASSERT(cond, msg) CHPP_STATIC_ASSERT2(cond, __LINE__)
-
-#endif  // CHPP_C11_OR_NEWER
-
-// Time-related macros
-#define CHPP_TIME_NONE 0
-#define CHPP_MSEC_PER_SEC UINT64_C(1000)
-#define CHPP_USEC_PER_MSEC UINT64_C(1000)
-#define CHPP_NSEC_PER_USEC UINT64_C(1000)
-#define CHPP_USEC_PER_SEC (CHPP_USEC_PER_MSEC * CHPP_MSEC_PER_SEC)
-#define CHPP_NSEC_PER_MSEC (CHPP_NSEC_PER_USEC * CHPP_USEC_PER_MSEC)
-#define CHPP_NSEC_PER_SEC \
-  (CHPP_NSEC_PER_USEC * CHPP_USEC_PER_MSEC * CHPP_MSEC_PER_SEC)
-
-#if defined(__GNUC__) || defined(__clang__)
-#define check_types_match(t1, t2) ((__typeof__(t1) *)0 != (__typeof__(t2) *)0)
-#else
-#define check_types_match(t1, t2) 0
-#endif
-
-#define container_of(ptr, type, member)                     \
-  ((type *)(void *)((char *)(ptr)-offsetof(type, member)) + \
-   check_types_match(*(ptr), ((type *)0)->member))
-
-#define sizeof_member(type, member) (sizeof(((type *)0)->member))
-
 /**
  * Macros for defining (compiler dependent) packed structures
  */
@@ -103,29 +51,15 @@ extern "C" {
 #define CHPP_PACKED_START
 #define CHPP_PACKED_END
 #define CHPP_PACKED_ATTR __attribute__((packed))
-
 #elif defined(__ICCARM__) || defined(__CC_ARM)
 // For IAR ARM and Keil MDK-ARM compilers
 #define CHPP_PACKED_START _Pragma("pack(push, 1)")
 #define CHPP_PACKED_END _Pragma("pack(pop)")
 #define CHPP_PACKED_ATTR
-
-#elif defined(_MSC_VER)
-// For Microsoft Visual Studio
-#define CHPP_PACKED_START __pragma(pack(push, 1))
-#define CHPP_PACKED_END __pragma(pack(pop))
-#define CHPP_PACKED_ATTR
-
 #else
 // Unknown compiler
 #error Unrecognized compiler
 #endif
-
-#define CHPP_FREE_AND_NULLIFY(p) \
-  do {                           \
-    chppFree(p);                 \
-    (p) = NULL;                  \
-  } while (0)
 
 #ifdef __cplusplus
 }
