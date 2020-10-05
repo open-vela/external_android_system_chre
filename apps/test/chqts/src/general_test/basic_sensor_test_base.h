03917c2dccec69e394ab35ac1c341048b92752f8
liujinye <liujinye@xiaomi.com>
2020-10-05 18:12:27 +0800

    Checksum Functionality and Unit Testing
    Bug: 146020579
    Test: atest --host chre_chpp_linux_tests -c
    
    Change-Id: Ic99a51471f8b640165a9c22bcb63db2fc373b6bd

--- apps/test/chqts/src/general_test/basic_sensor_test_base.h
+++ apps/test/chqts/src/general_test/basic_sensor_test_base.h
@@ -49,16 +49,6 @@ class BasicSensorTestBase : public Test {
   virtual uint8_t getSensorType() const = 0;
 
   /**
-   * Abstract method indicating if this sensor is required for a valid
-   * CHRE implementation.
-   *
-   * A CHRE isn't useful without certain sensors available.
-   *
-   * @returns true if this is sensor is required; false otherwise.
-   */
-  virtual bool isRequiredSensor() const = 0;
-
-  /**
    * Abstract method indicating if this is an on-change sensor.
    *
    * @returns true if this sensor is on-change; false otherwise.
@@ -119,9 +109,9 @@ class BasicSensorTestBase : public Test {
                        const chreSensorThreeAxisData *eventData);
   void handleSamplingChangeEvent(
       const chreSensorSamplingStatusEvent *eventData);
-  void handleSensorDataEvent(const void *eventData);
-  void sanityCheckHeader(const chreSensorDataHeader *header,
-                         bool modifyTimestamps, uint64_t eventDuration);
+  void handleSensorDataEvent(uint16_t eventType, const void *eventData);
+  void verifyEventHeader(const chreSensorDataHeader *header, uint16_t eventType,
+                         uint64_t eventDuration);
 };
 
 }  // namespace general_test
