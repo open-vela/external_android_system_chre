3e26415ff5a91a9ad8036cf83cc81cc5deb0a7d4
liujinye <liujinye@xiaomi.com>
2020-10-24 02:04:49 +0800

    Switch "system/core/base" ref to "system/libbase" am: 5b8f53fa8b
    Original change: https://android-review.googlesource.com/c/platform/system/chre/+/1471682
    
    Change-Id: Ic4cc7e8ba45b73c51a0795d8e650a016662b6e7d

--- platform/include/chre/platform/assert.h
+++ platform/include/chre/platform/assert.h
@@ -21,19 +21,23 @@
 
 /**
  * @file
- * Defines the CHRE_ASSERT and CHRE_ASSERT_LOG macros for CHRE platforms.
- * Platforms must supply an implementation for assertCondition or use the shared
- * implementation.
+ * Includes the platform-specific header file that supplies an assertion macro.
+ * The platform header must supply the following symbol as a macro or free
+ * function:
+ *
+ *   CHRE_ASSERT(scalar expression)
+ *
+ * Where expression will be checked to be false (ie: compares equal to zero) and
+ * terminate the program if found to be the case.
  */
 
 #if defined(CHRE_ASSERTIONS_ENABLED)
 
-#define CHRE_ASSERT(condition)                 \
-  do {                                         \
-    if (!(condition)) {                        \
-      chre::doAssert(CHRE_FILENAME, __LINE__); \
-    }                                          \
-  } while (0)
+#include "chre/target_platform/assert.h"
+
+#ifndef CHRE_ASSERT
+#error "CHRE_ASSERT must be defined by the target platform's assert.h"
+#endif  // CHRE_ASSERT
 
 #elif defined(CHRE_ASSERTIONS_DISABLED)
 
@@ -61,17 +65,5 @@
       CHRE_ASSERT(condition);                \
     }                                        \
   } while (0)
-
-namespace chre {
-
-/**
- * Performs assertion while logging the filename and line provided.
- *
- * @param filename The filename containing the assertion being fired.
- * @param line The line that contains the assertion being fired.
- */
-void doAssert(const char *filename, size_t line);
-
-}  // namespace chre
 
 #endif  // CHRE_PLATFORM_ASSERT_H_
