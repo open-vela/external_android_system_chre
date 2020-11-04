be22b543ba69ed78a5ee58f2240f5b915a83c3a1
openvela-robot <openvela-robot@xiaomi.com>
2020-11-04 18:34:34 +0800

    [automerger skipped] Merge rvc-release RP1A.201105.002 to stage-aosp-master - DO NOT MERGE am: 14a23f9209 -s ours
    am skip reason: Change-Id I1fbe7921158360e1160d4198a4d8bb7e0cd57958 with SHA-1 db91f328b5 is in history
    
    Original change: https://googleplex-android-review.googlesource.com/c/platform/system/chre/+/12984134
    
    Change-Id: Ic74f52ce382c2d159e740a584ceff91336ac4a5b

--- host/common/include/chre_host/fragmented_load_transaction.h
+++ host/common/include/chre_host/fragmented_load_transaction.h
@@ -34,25 +34,22 @@ struct FragmentedLoadRequest {
   uint32_t transactionId;
   uint64_t appId;
   uint32_t appVersion;
-  uint32_t appFlags;
   uint32_t targetApiVersion;
   size_t appTotalSizeBytes;
   std::vector<uint8_t> binary;
 
   FragmentedLoadRequest(size_t fragmentId, uint32_t transactionId,
                         const std::vector<uint8_t> &binary)
-      : FragmentedLoadRequest(fragmentId, transactionId, 0, 0, 0, 0, 0,
-                              binary) {}
+      : FragmentedLoadRequest(fragmentId, transactionId, 0, 0, 0, 0, binary) {}
 
   FragmentedLoadRequest(size_t fragmentId, uint32_t transactionId,
-                        uint64_t appId, uint32_t appVersion, uint32_t appFlags,
+                        uint64_t appId, uint32_t appVersion,
                         uint32_t targetApiVersion, size_t appTotalSizeBytes,
                         const std::vector<uint8_t> &binary)
       : fragmentId(fragmentId),
         transactionId(transactionId),
         appId(appId),
         appVersion(appVersion),
-        appFlags(appFlags),
         targetApiVersion(targetApiVersion),
         appTotalSizeBytes(appTotalSizeBytes),
         binary(binary) {}
@@ -72,14 +69,12 @@ class FragmentedLoadTransaction {
    * @param transactionId the unique ID of the unfragmented load transaction
    * @param appId the unique ID of the nanoapp
    * @param appVersion the version of the nanoapp
-   * @param appFlags the flags specified by the nanoapp to be loaded.
    * @param targetApiVersion the API version this nanoapp is targeted for
    * @param appBinary the nanoapp binary data
    * @param fragmentSize the size of each fragment in bytes
    */
   FragmentedLoadTransaction(uint32_t transactionId, uint64_t appId,
-                            uint32_t appVersion, uint32_t appFlags,
-                            uint32_t targetApiVersion,
+                            uint32_t appVersion, uint32_t targetApiVersion,
                             const std::vector<uint8_t> &appBinary,
                             size_t fragmentSize = kDefaultFragmentSize);
 
