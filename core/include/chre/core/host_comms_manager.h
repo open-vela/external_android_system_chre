6ca15ce4cfc2e116507929299583145df1eacff9
openvela-robot <openvela-robot@xiaomi.com>
2020-11-23 13:14:50 +0800

    Merge rvc-qpr-dev-plus-aosp-without-vendor@6881855
    Bug: 172690556
    Merged-In: I031d378543b3e134a686c0b075fbfae54f54873d
    Change-Id: I79a574a90b206cebd1201a726858f1f9700de816

--- core/include/chre/core/host_comms_manager.h
+++ core/include/chre/core/host_comms_manager.h
@@ -76,15 +76,28 @@ typedef HostMessage MessageFromHost;
 typedef HostMessage MessageToHost;
 
 /**
- * Common code for managing bi-directional communications between the host and
- * nanoapps. Inherits from the platform-specific HostLink class to accomplish
- * this, and also to provide an access point (lookup via the EventLoopManager
- * Singleton) to the platform-specific HostLinkBase functionality for use by
- * platform-specific code.
+ * Manages bi-directional communications with the host. There must only be one
+ * instance of this class per CHRE instance, as the HostLink is not multiplexed
+ * per-EventLoop.
  */
-class HostCommsManager : public HostLink {
+class HostCommsManager : public NonCopyable {
  public:
   HostCommsManager() : mIsNanoappBlamedForWakeup(false) {}
+
+  /**
+   * @see HostLink::flushMessagesSentByNanoapp
+   */
+  void flushMessagesSentByNanoapp(uint64_t appId);
+
+  /**
+   * Sends a Log Message to the host over the HostLink
+   * @see HostLink::sendLogMessage
+   *
+   * @param logMessage Buffer containing a (possibly encoded) log message
+   * @param logMessageSize size in bytes of the logMessage buffer
+   */
+
+  void sendLogMessage(const char *logMessage, size_t logMessageSize);
 
   /**
    * Formulates a MessageToHost using the supplied message contents and passes
@@ -177,6 +190,9 @@ class HostCommsManager : public HostLink {
   //! messages directly in onMessageToHostComplete.
   SynchronizedMemoryPool<HostMessage, kMaxOutstandingMessages> mMessagePool;
 
+  //! The platform-specific link to the host that we manage
+  HostLink mHostLink;
+
   /**
    * Allocates and populates the event structure used to notify a nanoapp of an
    * incoming message from the host.
@@ -204,9 +220,6 @@ class HostCommsManager : public HostLink {
    *
    * @param craftedMessage Message from host to be delivered to the destination
    * nanoapp
-   *
-   * @return true if the message was delivered to the event queue (i.e.
-   *         destination app ID exists in the system)
    */
   bool deliverNanoappMessageFromHost(MessageFromHost *craftedMessage);
 
