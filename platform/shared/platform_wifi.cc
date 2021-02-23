1d6653ab8f687e1827af55519c3842c5fca22822
openvela-robot <openvela-robot@xiaomi.com>
2021-02-23 09:05:17 +0800

    [automerger skipped] Merge ab/7061308 into stage. am: b93a9f52a3 -s ours
    am skip reason: Change-Id I4ace912e7bd46bcf7782eda6675da22b52b692c7 with SHA-1 db981ad273 is in history
    
    Original change: https://googleplex-android-review.googlesource.com/c/platform/system/chre/+/13658588
    
    MUST ONLY BE SUBMITTED BY AUTOMERGER
    
    Change-Id: Ifd8406dce76e6c5ae43ed92bb0bcc1e8aaf3e696

--- platform/shared/platform_wifi.cc
+++ platform/shared/platform_wifi.cc
@@ -21,7 +21,6 @@
 #include "chre/core/event_loop_manager.h"
 #include "chre/platform/log.h"
 #include "chre/platform/shared/pal_system_api.h"
-#include "chre/util/system/wifi_util.h"
 
 namespace chre {
 
@@ -88,15 +87,7 @@ bool PlatformWifi::requestRanging(const struct chreWifiRangingParams *params) {
 bool PlatformWifi::requestScan(const struct chreWifiScanParams *params) {
   if (mWifiApi != nullptr) {
     prePalApiCall();
-
-    // TODO(174240925): remove the version check override
-    if (true || mWifiApi->moduleVersion < CHRE_PAL_WIFI_API_V1_5) {
-      const struct chreWifiScanParams paramsCompat =
-          translateToLegacyWifiScanParams(params);
-      return mWifiApi->requestScan(&paramsCompat);
-    } else {
-      return mWifiApi->requestScan(params);
-    }
+    return mWifiApi->requestScan(params);
   } else {
     return false;
   }
