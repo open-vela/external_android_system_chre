d677c82adffe3e10ddda97799e228daa6bd87951
openvela-robot <openvela-robot@xiaomi.com>
2021-03-02 15:49:59 +0800

    Merge RQ2A.210305.007
    Bug: 180401296
    Merged-In: Ib5b977be3567158b83c3a7d7c1658b0fad4efc5b
    Change-Id: I2b7ea5ada3b1a5f34e81e1e4ccb43c07f11dc25c

--- core/debug_dump_manager.cc
+++ core/debug_dump_manager.cc
@@ -24,7 +24,7 @@
 namespace chre {
 
 void DebugDumpManager::trigger() {
-  auto callback = [](uint16_t /*type*/, void * /*data*/, void * /*extraData*/) {
+  auto callback = [](uint16_t /*eventType*/, void * /*eventData*/) {
     DebugDumpManager &debugDumpManager =
         EventLoopManagerSingleton::get()->getDebugDumpManager();
     debugDumpManager.collectFrameworkDebugDumps();
@@ -90,7 +90,6 @@ void DebugDumpManager::collectFrameworkDebugDumps() {
   eventLoopManager->getAudioRequestManager().logStateToBuffer(mDebugDump);
 #endif  // CHRE_AUDIO_SUPPORT_ENABLED
   logSettingStateToBuffer(mDebugDump);
-  logStateToBuffer(mDebugDump);
 }
 
 void DebugDumpManager::sendFrameworkDebugDumps() {
