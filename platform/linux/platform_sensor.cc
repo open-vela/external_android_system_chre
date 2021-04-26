fe974e1e277527157f00fa147dc69d3e305bf646
openvela-robot <openvela-robot@xiaomi.com>
2021-04-26 18:59:33 +0800

    Update clang used in build/arch/x86.mk to clang-r416183b am: 8de436ae60
    Original change: https://android-review.googlesource.com/c/platform/system/chre/+/1685995
    
    Change-Id: Ifccced5ef487f4a095616da1545eb54a2ba9d9e1

--- platform/linux/platform_sensor.cc
+++ platform/linux/platform_sensor.cc
@@ -38,10 +38,6 @@ const char *PlatformSensor::getSensorName() const {
   return mSensorInfo->sensorName;
 }
 
-uint8_t PlatformSensor::getSensorIndex() const {
-  return CHRE_SENSOR_INDEX_DEFAULT;
-}
-
 PlatformSensor::PlatformSensor(PlatformSensor &&other) {
   *this = std::move(other);
 }
