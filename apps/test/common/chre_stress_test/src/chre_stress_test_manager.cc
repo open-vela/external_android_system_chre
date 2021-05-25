f4ea0f4d0f014e5957539eefd677f4bf7d054dc9
liujinye <liujinye@xiaomi.com>
2021-05-25 22:24:00 +0800

    Fix retx book-keeping logic am: a7c3923c07
    Original change: https://googleplex-android-review.googlesource.com/c/platform/system/chre/+/14720792
    
    Change-Id: I3bc667f132a4a4c1cd603cc67b0b454a3d45e597

--- apps/test/common/chre_stress_test/src/chre_stress_test_manager.cc
+++ apps/test/common/chre_stress_test/src/chre_stress_test_manager.cc
@@ -246,10 +246,7 @@ void Manager::setTimer(uint64_t delayNs, bool oneShot, uint32_t *timerHandle) {
 void Manager::cancelTimer(uint32_t *timerHandle) {
   if (*timerHandle != CHRE_TIMER_INVALID) {
     if (!chreTimerCancel(*timerHandle)) {
-      // We don't treat this as a test failure, because the CHRE API does not
-      // guarantee this method succeeds (e.g. if the timer is one-shot and just
-      // fired).
-      LOGW("Failed to cancel timer");
+      logAndSendFailure("Failed to cancel timer");
     }
     *timerHandle = CHRE_TIMER_INVALID;
   }
