49831823005f7ddf02870a9db01951182bb1c52a
openvela-robot <openvela-robot@xiaomi.com>
2021-06-14 16:01:55 +0800

    Fix basic wifi test scan type validation am: 3e4fc61e1e am: a28f8823bb am: b7c65cea57
    Original change: https://googleplex-android-review.googlesource.com/c/platform/system/chre/+/14949626
    
    Change-Id: I2a21872055384e41a4b6a98cf06834f310b4294b

--- chpp/clients/loopback.c
+++ chpp/clients/loopback.c
@@ -88,7 +88,6 @@ bool chppDispatchLoopbackServiceResponse(struct ChppAppState *context,
   CHPP_NOT_NULL(context->loopbackClientContext->loopbackData);
 
   CHPP_ASSERT(chppClientTimestampResponse(
-      &context->loopbackClientContext->client,
       &context->loopbackClientContext->runLoopbackTest,
       (const struct ChppAppHeader *)response));
 
