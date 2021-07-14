cb5fa4952ce5f7f9784421d1d4a525a6a4419c16
openvela-robot <openvela-robot@xiaomi.com>
2021-07-14 20:27:09 +0800

    Add FreeRTOS support to CHRE
    This commit merges code that can be used as part of
    implementing support for CHRE on a platform that is built
    on top of FreeRTOS.
    
    It includes support for:
    - atomics
    - mutexes
    - CHRE init/deinit
    - dynamic loading / nanoapp authentication
    - debug dumps
    - optional DRAM voting support
    
    Bug: 193662098
    Test: Compile CHRE
    Change-Id: Ib10ee452abe3e512dc86eba2f7368c006714580d
    Merged-In: Ib10ee452abe3e512dc86eba2f7368c006714580d

--- apps/test/common/chre_cross_validator_sensor/inc/chre_cross_validator_sensor_manager.h
+++ apps/test/common/chre_cross_validator_sensor/inc/chre_cross_validator_sensor_manager.h
@@ -32,9 +32,6 @@ namespace cross_validator_sensor {
 // TODO(b/154271551): Break up the Manager class into more fine-grained classes
 // to avoid it becoming to complex.
 
-//! The maximum size of a sensor name.
-constexpr size_t kMaxSensorNameSize = 128;
-
 /**
  * Class to manage a CHRE cross validator nanoapp.
  */
@@ -102,9 +99,6 @@ class Manager {
   //! message.
   chre::Optional<CrossValidatorState> mCrossValidatorState;
 
-  //! A temporary global buffer where the sensor name is stored.
-  char mSensorNameArray[kMaxSensorNameSize];
-
   /**
    * Make a SensorDatapoint proto message.
    *
@@ -167,18 +161,6 @@ class Manager {
                                               void *const *arg);
 
   /**
-   * Encodes the datapoints into a SensorData message.
-   *
-   * @param stream The stream to write to.
-   * @param field The field to write to.
-   * @param arg The data passed in order to write to the stream.
-   * @return true if successful.
-   */
-  static bool encodeStepCounterSensorDatapoints(pb_ostream_t *stream,
-                                                const pb_field_t *field,
-                                                void *const *arg);
-
-  /**
    * Encodes a single float value into values list of SensorDatapoint object.
    *
    * @param stream The stream to write to.
@@ -205,19 +187,6 @@ class Manager {
                                                   void *const *arg);
 
   /**
-   * Encodes a single step counter value into the values list of SensorDatapoint
-   * object, converting the uint64 value property into a float in the process.
-   *
-   * @param stream The stream to write to.
-   * @param field The field to write to (unused).
-   * @param arg The data passed in order to write to the stream.
-   * @return true if successful.
-   */
-  static bool encodeStepCounterSensorDatapointValue(pb_ostream_t *stream,
-                                                    const pb_field_t *field,
-                                                    void *const *arg);
-
-  /**
    * Handle a start sensor message.
    *
    * @param startSensorCommand The StartSensorCommand proto message received.
@@ -238,20 +207,9 @@ class Manager {
   /**
    * Handle a start message from CHRE with the given data from host.
    *
-   * @param hostEndpoint The host endpoint the data was sent from.
    * @param hostData The data from host that has a start message.
    */
-  void handleStartMessage(uint16_t hostEndpoint,
-                          const chreMessageFromHostData *hostData);
-
-  /**
-   * Handle an info message from CHRE with the given data from host.
-   *
-   * @param hostEndpoint The host endpoint the data was sent from.
-   * @param hostData The data from host that has a start message.
-   */
-  void handleInfoMessage(uint16_t hostEndpoint,
-                         const chreMessageFromHostData *hostData);
+  void handleStartMessage(const chreMessageFromHostData *hostData);
 
   /**
    * Handle a message from the host.
@@ -293,16 +251,6 @@ class Manager {
       const chreSensorByteData *proximityDataFromChre);
 
   /**
-   * @param stepCounterDataFromChre Proximity sensor data from CHRE.
-   * @param sensorType The sensor type that sent the uint64 data.
-   *
-   * @return The Data proto message that is ready to be sent to host with float
-   * data.
-   */
-  chre_cross_validation_sensor_Data makeSensorStepCounterData(
-      const chreSensorUint64Data *stepCounterDataFromChre);
-
-  /**
    * Handle sensor three axis data from CHRE.
    *
    * @param threeAxisDataFromChre The data from CHRE to parse.
@@ -328,42 +276,11 @@ class Manager {
   void handleProximityData(const chreSensorByteData *proximityDataFromChre);
 
   /**
-   * Send data to be validated to the host.
-   * Handle step counter sensor data from CHRE.
-   *
-   * @param stepCounterDataFromChre The data to parse.
-   */
-  void handleStepCounterData(
-      const chreSensorUint64Data *stepCounterDataFromChre);
-
-  /**
    * Encode and send data to be validated to host.
    *
-   * @param data The data to send.
+   * @param data The data to encode and send.
    */
-  void sendDataToHost(const chre_cross_validation_sensor_Data &data);
-
-  /**
-   * Encode and send the info response to the host.
-   *
-   * @param hostEndpoint The endpoint to send the response to.
-   * @param infoResponse The info response to be encoded and sent.
-   */
-  void sendInfoResponse(
-      uint16_t hostEndpoint,
-      const chre_cross_validation_sensor_SensorInfoResponse &infoResponse);
-
-  /**
-   * Sends the provided message to the host.
-   *
-   * @param hostEndpoint The endpoint to send the message to.
-   * @param messageType The type of message being sent to the host.
-   * @param fields The fields of the provided struct that should be encoded.
-   * @param srcStruct The struct that should be encoded prior to sending to the
-   *     host.
-   */
-  void sendMessageToHost(uint16_t hostEndpoint, uint16_t messageType,
-                         const pb_field_t fields[], const void *srcStruct);
+  void encodeAndSendDataToHost(const chre_cross_validation_sensor_Data &data);
 
   /**
    * Determine if nanoapp is ready to process new sensor data.
@@ -389,16 +306,6 @@ class Manager {
    * during validation.
    */
   void cleanup();
-
-  /**
-   * @param sensorType The CHRE sensor type.
-   * @param sensorIndex The CHRE sensor index as defined in chreSensorFind.
-   * @param handle A non-null pointer where the sensor handle is stored, if
-   * found.
-   *
-   * @return true if the sensor corresponding to the input is available.
-   */
-  bool getSensor(uint32_t sensorType, uint32_t sensorIndex, uint32_t *handle);
 };
 
 // The chre cross validator manager singleton.
