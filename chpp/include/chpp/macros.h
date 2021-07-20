e3774e42a388658a0c1deaff55930eeef8a810c2
openvela-robot <openvela-robot@xiaomi.com>
2021-07-20 21:38:06 +0800

    Harden CHPP Against Datagrams w/ Invalid Handle Numbers am: a8009b1d46
    Original change: https://googleplex-android-review.googlesource.com/c/platform/system/chre/+/15287635
    
    Change-Id: Ie485134c1a1582b22ff88284e853aa3cb554acc5

--- chpp/include/chpp/macros.h
+++ chpp/include/chpp/macros.h
@@ -65,16 +65,9 @@ extern "C" {
 #define CHPP_NOT_NULL(var) CHPP_ASSERT((var) != NULL)
 #endif
 
-/**
- * Debug assertions that are not intended to be enabled for production software.
- */
 #ifndef CHPP_DEBUG_ASSERT
-#ifdef CHPP_DEBUG_ASSERT_ENABLED
 #define CHPP_DEBUG_ASSERT(var) CHPP_ASSERT(var)
-#else
-#define CHPP_DEBUG_ASSERT(var) UNUSED_VAR(var)
-#endif  // CHPP_DEBUG_ASSERT_ENABLED
-#endif  // CHPP_DEBUG_ASSERT
+#endif
 
 #ifndef PRIu64  // Pre-C99 lacks PRIu64 support. Note that the correct
                 // definition on pre-C99 systems would be compiler-dependent.
