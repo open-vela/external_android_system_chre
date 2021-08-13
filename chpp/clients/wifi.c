11854953134f01f6647555ef911ad94d66e5b1ab
liujinye <liujinye@xiaomi.com>
2021-08-14 06:31:37 +0800

    Merge sc-dev-plus-aosp-without-vendor@7634622
    Merged-In: I4ba298f408805444a87bd24572a642dce9309e99
    Change-Id: I3541c8c54b7771e8d71120e39444de1183960329

--- chpp/clients/wifi.c
+++ chpp/clients/wifi.c
@@ -365,7 +365,8 @@ static void chppWiFiRecoverScanMonitor(
 
     if (!chppWifiClientConfigureScanMonitor(true)) {
       clientContext->scanMonitorSilenceCallback = false;
-      CHPP_DEBUG_ASSERT_LOG(false, "Failed to re-enable WiFi scan monitoring");
+      CHPP_DEBUG_ASSERT_LOG(
+          false, "Unable to re-enable WiFi scan monitoring after reset");
     }
   }
 }
@@ -399,7 +400,8 @@ static void chppWifiCloseResult(struct ChppWifiClientState *clientContext,
 static void chppWifiGetCapabilitiesResult(
     struct ChppWifiClientState *clientContext, uint8_t *buf, size_t len) {
   if (len < sizeof(struct ChppWifiGetCapabilitiesResponse)) {
-    CHPP_LOGE("Bad WiFi capabilities len=%" PRIuSIZE, len);
+    struct ChppAppHeader *rxHeader = (struct ChppAppHeader *)buf;
+    CHPP_LOGE("GetCapabilities resp. too short. err=%" PRIu8, rxHeader->error);
 
   } else {
     struct ChppWifiGetCapabilitiesParameters *result =
@@ -408,9 +410,11 @@ static void chppWifiGetCapabilitiesResult(
     CHPP_LOGD("chppWifiGetCapabilitiesResult received capabilities=0x%" PRIx32,
               result->capabilities);
 
+#ifdef CHPP_WIFI_DEFAULT_CAPABILITIES
     CHPP_ASSERT_LOG((result->capabilities == CHPP_WIFI_DEFAULT_CAPABILITIES),
-                    "WiFi capabilities 0x%" PRIx32 " != 0x%" PRIx32,
+                    "Unexpected capability 0x%" PRIx32 " != 0x%" PRIx32,
                     result->capabilities, CHPP_WIFI_DEFAULT_CAPABILITIES);
+#endif
 
     clientContext->capabilities = result->capabilities;
   }
@@ -431,8 +435,15 @@ static void chppWifiConfigureScanMonitorResult(
 
   if (len < sizeof(struct ChppWifiConfigureScanMonitorAsyncResponse)) {
     // Short response length indicates an error
+
+    struct ChppAppHeader *rxHeader = (struct ChppAppHeader *)buf;
+    CHPP_LOGE("ScanMonitor resp. too short. err=%" PRIu8, rxHeader->error);
+
+    if (rxHeader->error == CHPP_APP_ERROR_NONE) {
+      rxHeader->error = CHPP_APP_ERROR_INVALID_LENGTH;
+    }
     gCallbacks->scanMonitorStatusChangeCallback(
-        false, chppAppShortResponseErrorHandler(buf, len, "ScanMonitor"));
+        false, chppAppErrorToChreError(rxHeader->error));
 
   } else {
     struct ChppWifiConfigureScanMonitorAsyncResponseParameters *result =
@@ -473,13 +484,20 @@ static void chppWifiRequestScanResult(struct ChppWifiClientState *clientContext,
 
   if (len < sizeof(struct ChppWifiRequestScanResponse)) {
     // Short response length indicates an error
-    gCallbacks->scanResponseCallback(
-        false, chppAppShortResponseErrorHandler(buf, len, "ScanRequest"));
+
+    struct ChppAppHeader *rxHeader = (struct ChppAppHeader *)buf;
+    CHPP_LOGE("ScanRequest resp. too short. err=%" PRIu8, rxHeader->error);
+
+    if (rxHeader->error == CHPP_APP_ERROR_NONE) {
+      rxHeader->error = CHPP_APP_ERROR_INVALID_LENGTH;
+    }
+    gCallbacks->scanResponseCallback(false,
+                                     chppAppErrorToChreError(rxHeader->error));
 
   } else {
     struct ChppWifiRequestScanResponseParameters *result =
         &((struct ChppWifiRequestScanResponse *)buf)->params;
-    CHPP_LOGI("Scan request success=%d at service", result->pending);
+    CHPP_LOGI("Scan request success=%d (at service)", result->pending);
     gCallbacks->scanResponseCallback(result->pending, result->errorCode);
   }
 }
@@ -501,7 +519,7 @@ static void chppWifiRequestRangingResult(
   struct ChppAppHeader *rxHeader = (struct ChppAppHeader *)buf;
 
   if (rxHeader->error != CHPP_APP_ERROR_NONE) {
-    CHPP_LOGE("Ranging failed at service" PRIu8);
+    CHPP_LOGE("RangingRequest failed at service err=%" PRIu8, rxHeader->error);
     gCallbacks->rangingEventCallback(chppAppErrorToChreError(rxHeader->error),
                                      NULL);
 
@@ -531,7 +549,7 @@ static void chppWifiScanEventNotification(
       chppWifiScanEventToChre((struct ChppWifiScanEvent *)buf, len);
 
   if (chre == NULL) {
-    CHPP_LOGE("Scan event conversion failed len=%" PRIuSIZE, len);
+    CHPP_LOGE("Scan event conversion failed: len=%" PRIuSIZE, len);
   } else {
 #ifdef CHPP_CLIENT_ENABLED_TIMESYNC
     uint64_t correctedTime =
@@ -628,10 +646,10 @@ static bool chppWifiClientOpen(const struct chrePalSystemApi *systemApi,
         /*blocking=*/true);
   }
 
-  // Since CHPP_WIFI_DEFAULT_CAPABILITIES is mandatory, we can always
-  // pseudo-open and return true. Otherwise, these should have been gated.
+#ifdef CHPP_WIFI_CLIENT_OPEN_ALWAYS_SUCCESS
   chppClientPseudoOpen(&gWifiClientContext.client);
   result = true;
+#endif
 
   return result;
 }
@@ -664,7 +682,11 @@ static void chppWifiClientClose(void) {
  * @return Capabilities flags.
  */
 static uint32_t chppWifiClientGetCapabilities(void) {
+#ifdef CHPP_WIFI_DEFAULT_CAPABILITIES
   uint32_t capabilities = CHPP_WIFI_DEFAULT_CAPABILITIES;
+#else
+  uint32_t capabilities = CHRE_WIFI_CAPABILITIES_NONE;
+#endif
 
   if (gWifiClientContext.capabilities != CHRE_WIFI_CAPABILITIES_NONE) {
     // Result already cached
