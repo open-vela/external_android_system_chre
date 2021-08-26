bed145b2ba3e85f3892f488ce72d0bdf58c9c411
liujinye <liujinye@xiaomi.com>
2021-08-26 17:45:57 +0800

    [automerger skipped] Don't use relative paths in CHQTS makefiles am: 403247fb78 am: f30536d18c -s ours
    am skip reason: Merged-In I9ae6a9c37a04077c3ae01095658f62ef759b4e71 with SHA-1 a4a9e34c69 is already in history
    
    Original change: https://googleplex-android-review.googlesource.com/c/platform/system/chre/+/15676740
    
    Change-Id: I45e0e2b096dcdfa9159931898a9ad081f1c9eeda

--- platform/linux/include/chre/platform/linux/pal_gnss.h
+++ platform/linux/include/chre/platform/linux/pal_gnss.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2017 The Android Open Source Project
+ * Copyright (C) 2021 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,20 +14,17 @@
  * limitations under the License.
  */
 
-#ifndef CHRE_PLATFORM_SLPI_SYSTEM_TIME_H_
-#define CHRE_PLATFORM_SLPI_SYSTEM_TIME_H_
-
-#include <cstdint>
-
-namespace chre {
+#ifndef CHRE_PLATFORM_LINUX_PAL_GNSS_H_
+#define CHRE_PLATFORM_LINUX_PAL_GNSS_H_
 
 /**
- * Sets the estimated offset between the host and SLPI clock.
- *
- * @param offset The current estimated offset in nanoseconds.
+ * @return true if the GNSS location session is enabled in the GNSS PAL.
  */
-void setEstimatedHostTimeOffset(int64_t offset);
+bool chrePalGnssIsLocationEnabled();
 
-}  // namespace chre
+/**
+ * @return true if the GNSS measurement session is enabled in the GNSS PAL.
+ */
+bool chrePalGnssIsMeasurementEnabled();
 
-#endif  // CHRE_PLATFORM_SLPI_SYSTEM_TIME_H_
+#endif  // CHRE_PLATFORM_LINUX_PAL_GNSS_H_
