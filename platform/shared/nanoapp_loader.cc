03cd7ba993f7077d5905c30ed37039302df76825
liujinye <liujinye@xiaomi.com>
2021-08-30 20:48:52 +0800

    [WifiXval|Host] Require exact AP v CHRE scan size. am: 8ecdeaf5a6 am: e1def8aefe
    Original change: https://googleplex-android-review.googlesource.com/c/platform/system/chre/+/15111730
    
    Change-Id: I88d48f00b331f48848a37586b514af4d4d4ad706

--- platform/shared/nanoapp_loader.cc
+++ platform/shared/nanoapp_loader.cc
@@ -390,9 +390,8 @@ bool NanoappLoader::callInitArray() {
   return success;
 }
 
-uintptr_t NanoappLoader::roundDownToAlign(uintptr_t virtualAddr,
-                                          size_t alignment) {
-  return virtualAddr & -alignment;
+uintptr_t NanoappLoader::roundDownToAlign(uintptr_t virtualAddr) {
+  return virtualAddr & -kBinaryAlignment;
 }
 
 void NanoappLoader::freeAllocatedData() {
@@ -670,16 +669,15 @@ bool NanoappLoader::createMappings() {
       // Get the last load segment
       while (last > first && last->p_type != PT_LOAD) --last;
 
-      size_t alignment = first->p_align;
       size_t memorySpan = last->p_vaddr + last->p_memsz - first->p_vaddr;
       LOGV("Nanoapp image Memory Span: %u", memorySpan);
 
       if (mIsTcmBinary) {
-        mMapping =
-            static_cast<uint8_t *>(nanoappBinaryAlloc(memorySpan, alignment));
+        mMapping = static_cast<uint8_t *>(
+            nanoappBinaryAlloc(memorySpan, kBinaryAlignment));
       } else {
         mMapping = static_cast<uint8_t *>(
-            nanoappBinaryDramAlloc(memorySpan, alignment));
+            nanoappBinaryDramAlloc(memorySpan, kBinaryAlignment));
       }
 
       if (mMapping == nullptr) {
@@ -688,8 +686,7 @@ bool NanoappLoader::createMappings() {
         LOGV("Starting location of mappings %p", mMapping);
 
         // Calculate the load bias using the first load segment.
-        uintptr_t adjustedFirstLoadSegAddr =
-            roundDownToAlign(first->p_vaddr, alignment);
+        uintptr_t adjustedFirstLoadSegAddr = roundDownToAlign(first->p_vaddr);
         mLoadBias =
             reinterpret_cast<uintptr_t>(mMapping) - adjustedFirstLoadSegAddr;
         LOGV("Load bias is %" PRIu32, mLoadBias);
