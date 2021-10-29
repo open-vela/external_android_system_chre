28548c22c7b4777a48be3097eb8ffcbfdc8e76f7
openvela-robot <openvela-robot@xiaomi.com>
2021-10-29 21:06:25 +0800

    Add exported symbols needed by nanoapps am: c1d6e65ada
    Original change: https://googleplex-android-review.googlesource.com/c/platform/system/chre/+/16148221
    
    Change-Id: I86342eb59ca368a580855ec97f3ee2d199213813

--- platform/slpi/host_link.cc
+++ platform/slpi/host_link.cc
@@ -34,6 +34,7 @@
 #endif
 #include "chre/platform/slpi/fastrpc.h"
 #include "chre/platform/slpi/power_control_util.h"
+#include "chre/platform/slpi/system_time.h"
 #include "chre/platform/system_time.h"
 #include "chre/platform/system_timer.h"
 #include "chre/util/fixed_size_blocking_queue.h"
@@ -920,7 +921,7 @@ void HostMessageHandlers::handleUnloadNanoappRequest(
 }
 
 void HostMessageHandlers::handleTimeSyncMessage(int64_t offset) {
-  SystemTime::setEstimatedHostTimeOffset(offset);
+  setEstimatedHostTimeOffset(offset);
 
   // Schedule a time sync request since offset may drift
   constexpr Seconds kClockDriftTimeSyncPeriod =
