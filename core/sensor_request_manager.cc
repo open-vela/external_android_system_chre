97a1b369544ca9d0d7c24a4d5ceb13d3ea0d2b7c
openvela-robot <openvela-robot@xiaomi.com>
2021-11-10 08:06:43 +0800

    Merge sc-qpr1-dev-plus-aosp-without-vendor@7810918
    Bug: 205056467
    Merged-In: I964bd5d3e718fe6aa355a2d43e3190da6d4cbbce
    Change-Id: Ie303695a4510860eebd7be6aadca368a0cce4b31

--- core/sensor_request_manager.cc
+++ core/sensor_request_manager.cc
@@ -196,21 +196,15 @@ bool SensorRequestManager::setSensorRequest(
           if (success) {
             cancelFlushRequests(sensorHandle, nanoapp->getInstanceId());
 
-            // Only unregister if nanoapp no longer has an outstanding request
-            // for a sensor + target group mask.
-            uint16_t activeMask =
-                getActiveTargetGroupMask(nanoapp->getInstanceId(), sensorType);
-            uint16_t inactiveMask = sensor.getTargetGroupMask() & ~activeMask;
-            if (inactiveMask != 0) {
-              nanoapp->unregisterForBroadcastEvent(eventType, inactiveMask);
+            nanoapp->unregisterForBroadcastEvent(eventType,
+                                                 sensor.getTargetGroupMask());
 
-              uint16_t biasEventType;
-              if (sensor.getBiasEventType(&biasEventType)) {
-                // Per API requirements, turn off bias reporting when
-                // unsubscribing from the sensor.
-                nanoapp->unregisterForBroadcastEvent(biasEventType,
-                                                     inactiveMask);
-              }
+            uint16_t biasEventType;
+            if (sensor.getBiasEventType(&biasEventType)) {
+              // Per API requirements, turn off bias reporting when
+              // unsubscribing from the sensor.
+              nanoapp->unregisterForBroadcastEvent(biasEventType,
+                                                   sensor.getTargetGroupMask());
             }
           }
         } else {
@@ -895,23 +889,6 @@ bool SensorRequestManager::configurePlatformSensor(
     }
   }
   return success;
-}
-
-uint16_t SensorRequestManager::getActiveTargetGroupMask(
-    uint32_t nanoappInstanceId, uint8_t sensorType) {
-  uint16_t mask = 0;
-  for (Sensor &sensor : mSensors) {
-    if (sensor.getSensorType() == sensorType) {
-      size_t index;
-      if (sensor.getRequestMultiplexer().findRequest(nanoappInstanceId,
-                                                     &index) != nullptr) {
-        mask |= sensor.getTargetGroupMask();
-        break;
-      }
-    }
-  }
-
-  return mask;
 }
 
 }  // namespace chre
