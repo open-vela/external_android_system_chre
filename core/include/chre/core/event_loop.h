eb628cc932027be28135c384dde57414943db656
liujinye <liujinye@xiaomi.com>
2021-11-10 08:06:43 +0800

    Merge sc-qpr1-dev-plus-aosp-without-vendor@7810918
    Bug: 205056467
    Merged-In: I964bd5d3e718fe6aa355a2d43e3190da6d4cbbce
    Change-Id: Ie303695a4510860eebd7be6aadca368a0cce4b31

--- core/include/chre/core/event_loop.h
+++ core/include/chre/core/event_loop.h
@@ -368,9 +368,6 @@ class EventLoop : public NonCopyable {
   //! The maximum number of events ever waiting in the event pool.
   size_t mMaxEventPoolUsage = 0;
 
-  //! The number of events dropped due to capacity limits
-  uint32_t mNumDroppedLowPriEvents = 0;
-
   /**
    * Modifies the run loop state so it no longer iterates on new events. This
    * should only be invoked by the event loop when it is ready to stop
@@ -392,9 +389,21 @@ class EventLoop : public NonCopyable {
                             uint16_t targetGroupMask);
 
   /**
-   * Delivers the next event pending to the Nanoapp.
+   * Do one round of Nanoapp event delivery, only considering events in
+   * Nanoapps' own queues (not mEvents).
+   *
+   * @return true if there are more events pending in Nanoapps' own queues
    */
-  void deliverNextEvent(const UniquePtr<Nanoapp> &app, Event *event);
+  bool deliverEvents();
+
+  /**
+   * Delivers the next event pending in the Nanoapp's queue, and takes care of
+   * freeing events once they have been delivered to all nanoapps. Must only be
+   * called after confirming that the app has at least 1 pending event.
+   *
+   * @return true if the nanoapp has another event pending in its queue
+   */
+  bool deliverNextEvent(const UniquePtr<Nanoapp> &app);
 
   /**
    * Given an event pulled from the main incoming event queue (mEvents), deliver
@@ -413,6 +422,11 @@ class EventLoop : public NonCopyable {
    * long as postEvent() will accept them.
    */
   void flushInboundEventQueue();
+
+  /**
+   * Delivers events pending in Nanoapps' own queues until they are all empty.
+   */
+  void flushNanoappEventQueues();
 
   /**
    * Call after when an Event has been delivered to all intended recipients.
