fd7bb5b633356f8310d9b1624aa20028752b4be5
liujinye <liujinye@xiaomi.com>
2021-11-18 22:37:54 +0800

    [automerger skipped] Merge sc-qpr1-dev-plus-aosp-without-vendor@7810918 am: 4eb1ee04de -s ours am: 7c644d55d8 -s ours am: 782fa62167 -s ours
    am skip reason: Merged-In I964bd5d3e718fe6aa355a2d43e3190da6d4cbbce with SHA-1 7f9a594a70 is already in history
    
    Original change: https://googleplex-android-review.googlesource.com/c/platform/system/chre/+/16276067
    
    Change-Id: Ic2d9babdc69bc6f801317719a98481f632cceda9

--- platform/slpi/host_link.cc
+++ platform/slpi/host_link.cc
@@ -34,7 +34,6 @@
 #endif
 #include "chre/platform/slpi/fastrpc.h"
 #include "chre/platform/slpi/power_control_util.h"
-#include "chre/platform/slpi/system_time.h"
 #include "chre/platform/system_time.h"
 #include "chre/platform/system_timer.h"
 #include "chre/util/fixed_size_blocking_queue.h"
@@ -283,7 +282,7 @@ int generateMessageToHost(const MessageToHost *msgToHost, unsigned char *buffer,
       builder, msgToHost->appId, msgToHost->toHostData.messageType,
       msgToHost->toHostData.hostEndpoint, msgToHost->message.data(),
       msgToHost->message.size(), msgToHost->toHostData.appPermissions,
-      msgToHost->toHostData.messagePermissions);
+      msgToHost->toHostData.messagePermissions, msgToHost->toHostData.wokeHost);
 
   int result = copyToHostBuffer(builder, buffer, bufferSize, messageLen);
 
@@ -921,7 +920,7 @@ void HostMessageHandlers::handleUnloadNanoappRequest(
 }
 
 void HostMessageHandlers::handleTimeSyncMessage(int64_t offset) {
-  setEstimatedHostTimeOffset(offset);
+  SystemTime::setEstimatedHostTimeOffset(offset);
 
   // Schedule a time sync request since offset may drift
   constexpr Seconds kClockDriftTimeSyncPeriod =
