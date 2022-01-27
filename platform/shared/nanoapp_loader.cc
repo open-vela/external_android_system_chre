ee860817cc39703030fbd50d0c41915661c186ea
openvela-robot <openvela-robot@xiaomi.com>
2022-01-27 19:17:13 +0800

    Reorder SettingState enum vals to match CHRE API am: 74e781fd1c am: 32df817547 am: d4f3c77cca am: 804a76db1b
    Original change: https://android-review.googlesource.com/c/platform/system/chre/+/1961642
    
    Change-Id: I6ff6bd0e2ab354b921ad359bfa9297038cad8370

--- platform/shared/nanoapp_loader.cc
+++ platform/shared/nanoapp_loader.cc
@@ -60,6 +60,17 @@ void deleteOverride(void *ptr) {
   FATAL_ERROR("Nanoapp tried to free %p through delete operator", ptr);
 }
 
+// From C++17, a call to the overloaded delete operator aimed at safely freeing
+// over-aligned allocations maybe present in the nanoapp binary even though it
+// is unused. Since all memory allocations/deallocations are managed by CHRE,
+// signal a fatal error if the nanoapp tries to use this version of the delete
+// operator.
+void deleteAlignedOverride(void *ptr, std::align_val_t alignment) {
+  UNUSED_VAR(alignment);
+
+  FATAL_ERROR("Nanoapp tried to free aligned %p via the delte operator", ptr);
+}
+
 // atexit is used to register functions that must be called when a binary is
 // removed from the system.
 int atexitOverride(void (*function)(void)) {
@@ -168,6 +179,7 @@ const ExportedData gExportedData[] = {
     ADD_EXPORTED_C_SYMBOL(__cxa_pure_virtual),
     ADD_EXPORTED_SYMBOL(atexitOverride, "atexit"),
     ADD_EXPORTED_SYMBOL(deleteOverride, "_ZdlPv"),
+    ADD_EXPORTED_SYMBOL(deleteAlignedOverride, "_ZdlPvSt11align_val_t"),
     ADD_EXPORTED_C_SYMBOL(dlsym),
     ADD_EXPORTED_C_SYMBOL(memcmp),
     ADD_EXPORTED_C_SYMBOL(memcpy),
@@ -192,6 +204,8 @@ const ExportedData gExportedData[] = {
     ADD_EXPORTED_C_SYMBOL(chreAbort),
     ADD_EXPORTED_C_SYMBOL(chreAudioConfigureSource),
     ADD_EXPORTED_C_SYMBOL(chreAudioGetSource),
+    ADD_EXPORTED_C_SYMBOL(chreBleGetCapabilities),
+    ADD_EXPORTED_C_SYMBOL(chreBleGetFilterCapabilities),
     ADD_EXPORTED_C_SYMBOL(chreConfigureDebugDumpEvent),
     ADD_EXPORTED_C_SYMBOL(chreConfigureHostSleepStateEvents),
     ADD_EXPORTED_C_SYMBOL(chreConfigureNanoappInfoEvents),
@@ -235,9 +249,15 @@ const ExportedData gExportedData[] = {
     ADD_EXPORTED_C_SYMBOL(chreWifiGetCapabilities),
     ADD_EXPORTED_C_SYMBOL(chreWifiRequestScanAsync),
     ADD_EXPORTED_C_SYMBOL(chreWifiRequestRangingAsync),
+    ADD_EXPORTED_C_SYMBOL(chreWifiNanRequestRangingAsync),
+    ADD_EXPORTED_C_SYMBOL(chreWifiNanSubscribe),
+    ADD_EXPORTED_C_SYMBOL(chreWifiNanSubscribeCancel),
     ADD_EXPORTED_C_SYMBOL(chreWwanGetCapabilities),
     ADD_EXPORTED_C_SYMBOL(chreWwanGetCellInfoAsync),
     ADD_EXPORTED_C_SYMBOL(platform_chreDebugDumpVaLog),
+    ADD_EXPORTED_C_SYMBOL(chreConfigureHostEndpointNotifications),
+    ADD_EXPORTED_C_SYMBOL(chrePublishRpcServices),
+    ADD_EXPORTED_C_SYMBOL(chreGetHostEndpointInfo),
 };
 CHRE_DEPRECATED_EPILOGUE
 // clang-format on
@@ -392,8 +412,9 @@ bool NanoappLoader::callInitArray() {
   return success;
 }
 
-uintptr_t NanoappLoader::roundDownToAlign(uintptr_t virtualAddr) {
-  return virtualAddr & -kBinaryAlignment;
+uintptr_t NanoappLoader::roundDownToAlign(uintptr_t virtualAddr,
+                                          size_t alignment) {
+  return virtualAddr & -alignment;
 }
 
 void NanoappLoader::freeAllocatedData() {
@@ -671,15 +692,16 @@ bool NanoappLoader::createMappings() {
       // Get the last load segment
       while (last > first && last->p_type != PT_LOAD) --last;
 
+      size_t alignment = first->p_align;
       size_t memorySpan = last->p_vaddr + last->p_memsz - first->p_vaddr;
       LOGV("Nanoapp image Memory Span: %u", memorySpan);
 
       if (mIsTcmBinary) {
-        mMapping = static_cast<uint8_t *>(
-            nanoappBinaryAlloc(memorySpan, kBinaryAlignment));
+        mMapping =
+            static_cast<uint8_t *>(nanoappBinaryAlloc(memorySpan, alignment));
       } else {
         mMapping = static_cast<uint8_t *>(
-            nanoappBinaryDramAlloc(memorySpan, kBinaryAlignment));
+            nanoappBinaryDramAlloc(memorySpan, alignment));
       }
 
       if (mMapping == nullptr) {
@@ -688,7 +710,8 @@ bool NanoappLoader::createMappings() {
         LOGV("Starting location of mappings %p", mMapping);
 
         // Calculate the load bias using the first load segment.
-        uintptr_t adjustedFirstLoadSegAddr = roundDownToAlign(first->p_vaddr);
+        uintptr_t adjustedFirstLoadSegAddr =
+            roundDownToAlign(first->p_vaddr, alignment);
         mLoadBias =
             reinterpret_cast<uintptr_t>(mMapping) - adjustedFirstLoadSegAddr;
         LOGV("Load bias is %" PRIu32, mLoadBias);
