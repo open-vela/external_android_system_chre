45d9edc15dc2f78b15ef178018ad7553a0bc468e
openvela-robot <openvela-robot@xiaomi.com>
2022-01-28 18:04:55 +0800

    [automerge] Add logf to list of exported symbols 2p: 70b1240e01
    Original change: https://googleplex-android-review.googlesource.com/c/platform/system/chre/+/16747412
    
    Bug: 216812062
    Bug: 216812645
    Change-Id: I77aeaf1d38af2ade06d332677a7ecae2ea6e782b

--- core/settings.cc
+++ core/settings.cc
@@ -149,8 +149,6 @@ void logSettingStateToBuffer(DebugDumpWrapper &debugDump) {
                   getSettingStateString(Setting::AIRPLANE_MODE));
   debugDump.print("\n Microphone Access %s",
                   getSettingStateString(Setting::MICROPHONE));
-  debugDump.print("\n BLE available %s",
-                  getSettingStateString(Setting::BLE_AVAILABLE));
 }
 
 }  // namespace chre
