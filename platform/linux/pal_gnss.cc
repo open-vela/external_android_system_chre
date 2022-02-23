fe584006e63e9af945584458a272de80291bfa11
liujinye <liujinye@xiaomi.com>
2022-02-23 20:48:29 +0800

    Cleanup WIFI subscriptions on nanoapp unload am: ca14b5af8e
    Original change: https://googleplex-android-review.googlesource.com/c/platform/system/chre/+/16946177
    
    Change-Id: Idf4daffab59a499ca7808fa1aa356859ad7c90d3

--- platform/linux/pal_gnss.cc
+++ platform/linux/pal_gnss.cc
@@ -106,8 +106,7 @@ void stopMeasurementThreads() {
 }
 
 uint32_t chrePalGnssGetCapabilities() {
-  return CHRE_GNSS_CAPABILITIES_LOCATION | CHRE_GNSS_CAPABILITIES_MEASUREMENTS |
-         CHRE_GNSS_CAPABILITIES_GNSS_ENGINE_BASED_PASSIVE_LISTENER;
+  return CHRE_GNSS_CAPABILITIES_LOCATION | CHRE_GNSS_CAPABILITIES_MEASUREMENTS;
 }
 
 bool chrePalControlLocationSession(bool enable, uint32_t minIntervalMs,
@@ -171,13 +170,6 @@ bool chrePalGnssApiOpen(const struct chrePalSystemApi *systemApi,
   return success;
 }
 
-bool gIsPassiveListenerEnabled = false;
-
-bool chrePalGnssconfigurePassiveLocationListener(bool enable) {
-  gIsPassiveListenerEnabled = enable;
-  return true;
-}
-
 }  // anonymous namespace
 
 bool chrePalGnssIsLocationEnabled() {
@@ -186,10 +178,6 @@ bool chrePalGnssIsLocationEnabled() {
 
 bool chrePalGnssIsMeasurementEnabled() {
   return gIsMeasurementEnabled;
-}
-
-bool chrePalGnssIsPassiveLocationListenerEnabled() {
-  return gIsPassiveListenerEnabled;
 }
 
 const struct chrePalGnssApi *chrePalGnssGetApi(uint32_t requestedApiVersion) {
@@ -202,8 +190,6 @@ const struct chrePalGnssApi *chrePalGnssGetApi(uint32_t requestedApiVersion) {
       .releaseLocationEvent = chrePalGnssReleaseLocationEvent,
       .controlMeasurementSession = chrePalControlMeasurementSession,
       .releaseMeasurementDataEvent = chrePalGnssReleaseMeasurementDataEvent,
-      .configurePassiveLocationListener =
-          chrePalGnssconfigurePassiveLocationListener,
   };
 
   if (!CHRE_PAL_VERSIONS_ARE_COMPATIBLE(kApi.moduleVersion,
