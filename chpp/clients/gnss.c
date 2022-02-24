125385dba485892daa96847c9405f3708d17814d
openvela-robot <openvela-robot@xiaomi.com>
2022-02-25 02:58:53 +0800

    Initialize CHPP service openState during registration am: 66c6e196a0
    Original change: https://googleplex-android-review.googlesource.com/c/platform/system/chre/+/17007025
    
    Change-Id: Ibe795df0d9d09e29895c97ef057d4e1e372c2861

--- chpp/clients/gnss.c
+++ chpp/clients/gnss.c
@@ -70,8 +70,6 @@ struct ChppGnssClientState {
   uint32_t capabilities;           // Cached GetCapabilities result
   bool requestStateResyncPending;  // requestStateResync() is waiting to be
                                    // processed
-  bool capabilitiesValid;  // Flag to indicate if the capabilities result
-                           // is valid
 };
 
 // Note: This global definition of gGnssClientContext supports only one
@@ -398,7 +396,6 @@ static void chppGnssGetCapabilitiesResult(
                     "GNSS capabilities 0x%" PRIx32 " != 0x%" PRIx32,
                     result->capabilities, CHPP_GNSS_DEFAULT_CAPABILITIES);
 
-    clientContext->capabilitiesValid = true;
     clientContext->capabilities = result->capabilities;
   }
 }
@@ -630,7 +627,6 @@ static void chppGnssClientClose(void) {
                  sizeof(*request))) {
     gGnssClientContext.client.openState = CHPP_OPEN_STATE_CLOSED;
     gGnssClientContext.capabilities = CHRE_GNSS_CAPABILITIES_NONE;
-    gGnssClientContext.capabilitiesValid = false;
     chppClientCloseOpenRequests(&gGnssClientContext.client, &kGnssClientConfig,
                                 true /* clearOnly */);
   }
@@ -645,7 +641,7 @@ static void chppGnssClientClose(void) {
 static uint32_t chppGnssClientGetCapabilities(void) {
   uint32_t capabilities = CHPP_GNSS_DEFAULT_CAPABILITIES;
 
-  if (gGnssClientContext.capabilitiesValid) {
+  if (gGnssClientContext.capabilities != CHRE_GNSS_CAPABILITIES_NONE) {
     // Result already cached
     capabilities = gGnssClientContext.capabilities;
 
@@ -661,9 +657,7 @@ static uint32_t chppGnssClientGetCapabilities(void) {
               &gGnssClientContext.rRState[CHPP_GNSS_GET_CAPABILITIES], request,
               sizeof(*request))) {
         // Success. gGnssClientContext.capabilities is now populated
-        if (gGnssClientContext.capabilitiesValid) {
-          capabilities = gGnssClientContext.capabilities;
-        }
+        capabilities = gGnssClientContext.capabilities;
       }
     }
   }
