23aec3a30bd5834a6ab992c61db148e8205aa35f
openvela-robot <openvela-robot@xiaomi.com>
2022-03-03 03:47:06 +0800

    Cleanup BLE scan on nanoapp unload am: ed91a05424
    Original change: https://googleplex-android-review.googlesource.com/c/platform/system/chre/+/17038021
    
    Change-Id: I18aef3aa047fc8693847dbe2ff068d8fa3078b17

--- core/include/chre/core/wifi_request_manager.h
+++ core/include/chre/core/wifi_request_manager.h
@@ -270,8 +270,10 @@ class WifiRequestManager : public NonCopyable {
    * Disables pending scan monitoring and NAN subscription for a nanoapp
    *
    * @param nanoapp A non-null pointer to the nanoapp.
+   *
+   * @return The number of disabled subscriptions.
    */
-  void disableAllSubscriptions(Nanoapp *nanoapp);
+  uint32_t disableAllSubscriptions(Nanoapp *nanoapp);
 
   /**
    * Get the number of current active NAN subscriptions.
