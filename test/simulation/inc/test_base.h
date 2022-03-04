546bd50053d57507caf462134965853b4bc3bdd6
liujinye <liujinye@xiaomi.com>
2022-03-04 23:32:09 +0800

    Clarify the doc for waitForEvent am: 0689a81bc4
    Original change: https://googleplex-android-review.googlesource.com/c/platform/system/chre/+/17072641
    
    Change-Id: I8d62c9b584629374773b891de34dcd5ca307f053

--- test/simulation/inc/test_base.h
+++ test/simulation/inc/test_base.h
@@ -21,7 +21,6 @@
 #include <cstdint>
 #include <thread>
 
-#include "chre/platform/system_timer.h"
 #include "chre/util/time.h"
 #include "test_event_queue.h"
 
@@ -75,7 +74,6 @@ class TestBase : public testing::Test {
   }
 
   std::thread mChreThread;
-  SystemTimer mSystemTimer;
 };
 
 }  // namespace chre
