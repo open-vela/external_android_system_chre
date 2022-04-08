cafb99c70391c32b19c776b0f0cb2d299f8e0e3b
openvela-robot <openvela-robot@xiaomi.com>
2022-04-08 15:28:31 +0800

    Add delete operator to the overrides am: 9531652082 am: a60c194684
    Original change: https://googleplex-android-review.googlesource.com/c/platform/system/chre/+/17622886
    
    Change-Id: I77737fc5386da8eabebeaa852db39df069f12c89
    Signed-off-by: Automerger Merge Worker <android-build-automerger-merge-worker@system.gserviceaccount.com>

--- chpp/clients/wwan.c
+++ chpp/clients/wwan.c
@@ -313,12 +313,9 @@ static void chppWwanGetCapabilitiesResult(
     CHPP_LOGD("chppWwanGetCapabilitiesResult received capabilities=0x%" PRIx32,
               result->capabilities);
 
-    CHPP_ASSERT((result->capabilities & CHPP_WWAN_DEFAULT_CAPABILITIES) ==
-                CHPP_WWAN_DEFAULT_CAPABILITIES);
-    if (result->capabilities != CHPP_WWAN_DEFAULT_CAPABILITIES) {
-      CHPP_LOGE("WWAN capabilities 0x%" PRIx32 " != 0x%" PRIx32,
-                result->capabilities, CHPP_WWAN_DEFAULT_CAPABILITIES);
-    }
+    CHPP_ASSERT_LOG((result->capabilities == CHPP_WWAN_DEFAULT_CAPABILITIES),
+                    "WWAN capabilities 0x%" PRIx32 " != 0x%" PRIx32,
+                    result->capabilities, CHPP_WWAN_DEFAULT_CAPABILITIES);
 
     clientContext->capabilitiesValid = true;
     clientContext->capabilities = result->capabilities;
