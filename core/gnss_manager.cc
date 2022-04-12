84c74f21e5df130d2bdf56dd4aaf9cfbf815b1f7
liujinye <liujinye@xiaomi.com>
2022-04-13 00:06:37 +0800

    Merge changes from topic "presubmit-am-886d700564804be3853c117797fa52db"
    * changes:
      [automerge] Fix run_pal_impl_tests.sh 2p: d0d8f203af 2p: fb62334230
      [automerge] Fix run_pal_impl_tests.sh 2p: d0d8f203af
      Fix run_pal_impl_tests.sh

--- core/gnss_manager.cc
+++ core/gnss_manager.cc
@@ -410,8 +410,7 @@ bool GnssSession::configure(Nanoapp *nanoapp, bool enable,
   uint16_t instanceId = nanoapp->getInstanceId();
   size_t requestIndex = 0;
   bool hasRequest = nanoappHasRequest(instanceId, &requestIndex);
-
-  if (asyncResponsePending()) {
+  if (!mStateTransitions.empty()) {
     success = addRequestToQueue(instanceId, enable, minInterval, cookie);
   } else if (stateTransitionIsRequired(enable, minInterval, hasRequest,
                                        requestIndex)) {
