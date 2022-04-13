4f16fe84bc66485fcb1a01a6327ddc7230248b67
liujinye <liujinye@xiaomi.com>
2022-04-13 15:21:40 +0800

    Ensure max one outstanding GNSS request am: f373a9cb0c
    Original change: https://googleplex-android-review.googlesource.com/c/platform/system/chre/+/17698819
    
    Change-Id: I4d11efa09cfdbb7da734560c88346ac4ae80fe14
    Signed-off-by: Automerger Merge Worker <android-build-automerger-merge-worker@system.gserviceaccount.com>

--- host/common/wifi_ext_hal_handler.cc
+++ host/common/wifi_ext_hal_handler.cc
@@ -16,21 +16,26 @@
 
 #include "chre_host/wifi_ext_hal_handler.h"
 
+#ifdef WIFI_EXT_V_1_3_HAS_MERGED
+
 namespace android {
 namespace chre {
 
 WifiExtHalHandler::~WifiExtHalHandler() {
-  notifyThreadToExit();
+  wifiExtHandlerThreadNotifyToExit();
   mThread.join();
 }
 
-WifiExtHalHandler::WifiExtHalHandler(
-    const std::function<void(bool)> &statusChangeCallback) {
+WifiExtHalHandler::WifiExtHalHandler() {
   mEnableConfig.reset();
   mThread = std::thread(&WifiExtHalHandler::wifiExtHandlerThreadEntry, this);
   auto cb = [&]() { onWifiExtHalServiceDeath(); };
   mDeathRecipient = new WifiExtHalDeathRecipient(cb);
-  mCallback = new WifiExtCallback(statusChangeCallback);
+}
+
+void WifiExtHalHandler::init(
+    const std::function<void(bool)> &statusChangeCallback) {
+  mStatusChangeCallback = std::move(statusChangeCallback);
 }
 
 void WifiExtHalHandler::handleConfigurationRequest(bool enable) {
@@ -47,11 +52,18 @@ void WifiExtHalHandler::dispatchConfigurationRequest(bool enable) {
            (enable == true) ? "Enable" : "Disable", status.code,
            status.description.c_str());
     }
-    mCallback->onStatusChanged(success);
+    onStatusChanged(success);
   };
 
   if (checkWifiExtHalConnected()) {
-    auto result = mService->requestWifiChreNanRtt(enable, hidlCb);
+    hardware::Return<void> result;
+    if (enable) {
+      // The transaction ID is inconsequential from CHRE's perspective, and is
+      // an unimplemented artifact in the WiFi ext HAL.
+      result = mService->enableWifiChreNan(0 /*transaction_id*/, hidlCb);
+    } else {
+      result = mService->disableWifiChreNan(0 /*transaction_id*/, hidlCb);
+    }
     if (!result.isOk()) {
       LOGE("Failed to %s NAN: %s", (enable == true) ? "Enable" : "Disable",
            result.description().c_str());
@@ -60,29 +72,15 @@ void WifiExtHalHandler::dispatchConfigurationRequest(bool enable) {
 }
 
 bool WifiExtHalHandler::checkWifiExtHalConnected() {
-  bool success = false;
+  bool success = true;
   if (mService == nullptr) {
     mService = IWifiExt::getService();
     if (mService != nullptr) {
       LOGD("Connected to Wifi Ext HAL service");
       mService->linkToDeath(mDeathRecipient, 0 /*cookie*/);
-
-      auto hidlCb = [&success](const WifiStatus &status) {
-        success = (status.code == WifiStatusCode::SUCCESS);
-        if (!success) {
-          LOGE("Failed to register CHRE callback with WifiExt: %s",
-               status.description.c_str());
-        }
-      };
-      auto result = mService->registerChreCallback(mCallback, hidlCb);
-      if (!result.isOk()) {
-        LOGE("Failed to register CHRE callback with WifiEmDeathRecipientxt: %s",
-             result.description().c_str());
-      } else {
-        success = true;
-      }
     } else {
       LOGE("Failed to connect to Wifi Ext HAL service");
+      success = false;
     }
   }
   return success;
@@ -111,7 +109,7 @@ void WifiExtHalHandler::wifiExtHandlerThreadEntry() {
   }
 }
 
-void WifiExtHalHandler::notifyThreadToExit() {
+void WifiExtHalHandler::wifiExtHandlerThreadNotifyToExit() {
   std::lock_guard<std::mutex> lock(mMutex);
   mThreadRunning = false;
   mCondVar.notify_one();
@@ -119,3 +117,5 @@ void WifiExtHalHandler::notifyThreadToExit() {
 
 }  // namespace chre
 }  // namespace android
+
+#endif  // WIFI_EXT_V_1_3_HAS_MERGED
