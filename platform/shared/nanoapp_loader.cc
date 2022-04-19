f514fbe759f99d3396342755bf05c75f588d0adc
liujinye <liujinye@xiaomi.com>
2022-04-19 16:40:17 +0800

    Fix a race condition in the GNSS sim tests am: 575382c4da am: 8d1a8b2b40
    Original change: https://googleplex-android-review.googlesource.com/c/platform/system/chre/+/17765530
    
    Change-Id: Id1b161fb9a3ed4d058e845fc20249b646a18facb
    Signed-off-by: Automerger Merge Worker <android-build-automerger-merge-worker@system.gserviceaccount.com>

--- platform/shared/nanoapp_loader.cc
+++ platform/shared/nanoapp_loader.cc
@@ -32,10 +32,6 @@
 #include "chre/util/dynamic_vector.h"
 #include "chre/util/macros.h"
 
-#ifdef CHRE_LOG_ATOM_EXTENSION_ENABLED
-#include "chrex_log_atom.h"
-#endif
-
 #ifndef CHRE_LOADER_ARCH
 #define CHRE_LOADER_ARCH EM_ARM
 #endif  // CHRE_LOADER_ARCH
@@ -146,7 +142,6 @@ void __cxa_pure_virtual(void) {
 // TODO(karthikmb/stange): While this array was hand-coded for simple
 // "hello-world" prototyping, the list of exported symbols must be
 // generated to minimize runtime errors and build breaks.
-// TODO(b/226455808): Allow extensions to this list via an external file
 // clang-format off
 // Disable deprecation warning so that deprecated symbols in the array
 // can be exported for older nanoapps and tests.
@@ -272,9 +267,6 @@ const ExportedData gExportedData[] = {
     ADD_EXPORTED_C_SYMBOL(chreConfigureHostEndpointNotifications),
     ADD_EXPORTED_C_SYMBOL(chrePublishRpcServices),
     ADD_EXPORTED_C_SYMBOL(chreGetHostEndpointInfo),
-#ifdef CHRE_LOG_ATOM_EXTENSION_ENABLED
-    ADD_EXPORTED_C_SYMBOL(chrexLogAtom),
-#endif
 };
 CHRE_DEPRECATED_EPILOGUE
 // clang-format on
