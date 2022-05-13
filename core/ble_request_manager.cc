a5388198c9c7e0e137f7bbc78e8e9163a0afb63c
liujinye <liujinye@xiaomi.com>
2022-05-14 05:17:17 +0800

    Snap for 8591930 from 8ece757cd9de73316441edc2a65404df6baa0150 to udc-release
    Change-Id: I9c8d2c7ba70ecf7171982cc287074b5f6bbcd9f2

--- core/ble_request_manager.cc
+++ core/ble_request_manager.cc
@@ -396,7 +396,9 @@ bool BleRequestManager::validateParams(const BleRequest &request) {
         valid = false;
         break;
       }
-      if (filter.len == 0 || filter.len > CHRE_BLE_DATA_LEN_MAX) {
+
+      uint8_t expectedLen = getFilterLenByAdType(filter.type);
+      if (expectedLen != filter.len) {
         valid = false;
         break;
       }
@@ -424,7 +426,23 @@ void BleRequestManager::postAsyncResultEventFatal(uint16_t instanceId,
 }
 
 bool BleRequestManager::isValidAdType(uint8_t adType) {
-  return adType == CHRE_BLE_AD_TYPE_SERVICE_DATA_WITH_UUID_16;
+  return adType == CHRE_BLE_FILTER_TYPE_SERVICE_DATA_UUID_16 ||
+         adType == CHRE_BLE_FILTER_TYPE_SERVICE_DATA_UUID_32 ||
+         adType == CHRE_BLE_FILTER_TYPE_SERVICE_DATA_UUID_128;
+}
+
+uint8_t BleRequestManager::getFilterLenByAdType(uint8_t adType) {
+  switch (adType) {
+    case CHRE_BLE_FILTER_TYPE_SERVICE_DATA_UUID_16:
+      return 2;
+    case CHRE_BLE_FILTER_TYPE_SERVICE_DATA_UUID_32:
+      return 4;
+    case CHRE_BLE_FILTER_TYPE_SERVICE_DATA_UUID_128:
+      return 16;
+    default:
+      CHRE_ASSERT(false);
+      return UINT8_MAX;
+  }
 }
 
 bool BleRequestManager::bleSettingEnabled() {
