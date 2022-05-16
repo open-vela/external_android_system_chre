84c240fa0ef9e67b9a76bdb67a5f0feef7385b07
liujinye <liujinye@xiaomi.com>
2022-05-16 22:07:35 +0800

    Fix disabling the scan monitoring am: c6549e2b7f am: e9dc89d0fc
    Original change: https://googleplex-android-review.googlesource.com/c/platform/system/chre/+/18408951
    
    Change-Id: I00bb1de97dfdcf3de0aeafd240e10139f6b684e8
    Signed-off-by: Automerger Merge Worker <android-build-automerger-merge-worker@system.gserviceaccount.com>

--- platform/shared/nanoapp_loader.cc
+++ platform/shared/nanoapp_loader.cc
@@ -32,10 +32,6 @@
 #include "chre/util/dynamic_vector.h"
 #include "chre/util/macros.h"
 
-#ifdef CHRE_LOG_ATOM_EXTENSION_ENABLED
-#include "chrex_log_atom.h"
-#endif
-
 #ifdef CHREX_VOTE_ENABLED
 #include "chrex_vote.h"
 #endif
@@ -150,7 +146,6 @@ void __cxa_pure_virtual(void) {
 // TODO(karthikmb/stange): While this array was hand-coded for simple
 // "hello-world" prototyping, the list of exported symbols must be
 // generated to minimize runtime errors and build breaks.
-// TODO(b/226455808): Allow extensions to this list via an external file
 // clang-format off
 // Disable deprecation warning so that deprecated symbols in the array
 // can be exported for older nanoapps and tests.
@@ -276,9 +271,6 @@ const ExportedData gExportedData[] = {
     ADD_EXPORTED_C_SYMBOL(chreConfigureHostEndpointNotifications),
     ADD_EXPORTED_C_SYMBOL(chrePublishRpcServices),
     ADD_EXPORTED_C_SYMBOL(chreGetHostEndpointInfo),
-#ifdef CHRE_LOG_ATOM_EXTENSION_ENABLED
-    ADD_EXPORTED_C_SYMBOL(chrexLogAtom),
-#endif
 #ifdef CHREX_VOTE_ENABLED
     ADD_EXPORTED_C_SYMBOL(chrexClockRateChangeRequest),
 #endif
