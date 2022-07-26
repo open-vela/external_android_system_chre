6767fe7741f72deb66aa71545c640b31462cbdfe
liujinye <liujinye@xiaomi.com>
2022-07-26 17:12:17 +0800

    zephyr: add "zephyr/" include prefixes am: f9dfe2b630 am: 55d1fe3b90
    Original change: https://android-review.googlesource.com/c/platform/system/chre/+/2160657
    
    Change-Id: Ic1052448e054d16aef54a57f8174dfbc61cedee8
    Signed-off-by: Automerger Merge Worker <android-build-automerger-merge-worker@system.gserviceaccount.com>

--- platform/zephyr/init.cc
+++ platform/zephyr/init.cc
@@ -17,7 +17,7 @@
 
 #include <errno.h>
 #include <zephyr/sys/printk.h>
-#include <zephyr/kernel.h>
+#include <zephyr/zephyr.h>
 
 #include "chre/core/event_loop_manager.h"
 #include "chre/core/static_nanoapps.h"
