b2e72e49dcd03d37861dd6d6d3fb50a65ed09d77
liujinye <liujinye@xiaomi.com>
2022-07-26 16:46:51 +0800

    zephyr: add "zephyr/" include prefixes am: f9dfe2b630
    Original change: https://android-review.googlesource.com/c/platform/system/chre/+/2160657
    
    Change-Id: I2596f7c0bb8eadbc252f856100d873574d58d317
    Signed-off-by: Automerger Merge Worker <android-build-automerger-merge-worker@system.gserviceaccount.com>

--- apps/test/common/chre_settings_test/inc/chre_settings_test_util.h
+++ apps/test/common/chre_settings_test/inc/chre_settings_test_util.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2022 The Android Open Source Project
+ * Copyright (C) 2020 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,24 +14,21 @@
  * limitations under the License.
  */
 
-#ifndef CHRE_HOST_FILE_STREAM_H_
-#define CHRE_HOST_FILE_STREAM_H_
+#ifndef CHRE_SETTINGS_TEST_UTIL_H_
+#define CHRE_SETTINGS_TEST_UTIL_H_
 
-#include <vector>
+#include <cinttypes>
 
-namespace android {
 namespace chre {
 
-/**
- * Reads a file and stores it into a buffer.
- *
- * @param filename The name of the file.
- * @param buffer The buffer to store the contents of the file into.
- * @return true if successfully read and stored.
- */
-bool readFileContents(const char *filename, std::vector<uint8_t> *buffer);
+namespace settings_test {
+
+void sendTestResultToHost(uint16_t hostEndpointId, bool success);
+
+void sendEmptyMessageToHost(uint16_t hostEndpointId, uint32_t messageType);
+
+}  // namespace settings_test
 
 }  // namespace chre
-}  // namespace android
 
-#endif  // CHRE_HOST_FILE_STREAM_H_
+#endif  // CHRE_SETTINGS_TEST_UTIL_H_
