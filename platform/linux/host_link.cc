ee4c500929c9dde662bffe76ce0beca494422de6
openvela-robot <openvela-robot@xiaomi.com>
2022-09-16 01:26:21 +0800

    Disable stack-use-after-return am: 12e3a5b43b
    Original change: https://android-review.googlesource.com/c/platform/system/chre/+/2218790
    
    Change-Id: I01ec2ea215dad34675f0ed09fdcd8466cfe81df7
    Signed-off-by: Automerger Merge Worker <android-build-automerger-merge-worker@system.gserviceaccount.com>

--- platform/linux/host_link.cc
+++ platform/linux/host_link.cc
@@ -20,15 +20,12 @@
 namespace chre {
 
 void HostLink::flushMessagesSentByNanoapp(uint64_t /* appId */) {
-  // (empty)
+  // TODO: implement
 }
 
-bool HostLink::sendMessage(const MessageToHost *message) {
-  // Just drop the message since we don't have a real host to send to
-  EventLoopManagerSingleton::get()
-      ->getHostCommsManager()
-      .onMessageToHostComplete(message);
-  return true;
+bool HostLink::sendMessage(const MessageToHost * /* message */) {
+  // TODO: implement
+  return false;
 }
 
 void HostLinkBase::sendNanConfiguration(bool enable) {
