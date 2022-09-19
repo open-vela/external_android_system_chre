e94843ad004a68fde1d5750b1a7c6d612b55ac3c
liujinye <liujinye@xiaomi.com>
2022-09-19 23:39:31 +0800

    Add null check for IStats service am: aa53647751
    Original change: https://googleplex-android-review.googlesource.com/c/platform/system/chre/+/19969342
    
    Change-Id: I019071a3af588c1d7431b86a64b9614ab2410e2b
    Signed-off-by: Automerger Merge Worker <android-build-automerger-merge-worker@system.gserviceaccount.com>

--- apps/test/chqts/src/general_test/timer_cancel_test.cc
+++ apps/test/chqts/src/general_test/timer_cancel_test.cc
@@ -23,9 +23,6 @@
 #include <shared/time_util.h>
 
 #include <chre.h>
-#include <chre/util/nanoapp/log.h>
-
-#define LOG_TAG "[TimerCancelTest]"
 
 using nanoapp_testing::kOneMillisecondInNanoseconds;
 using nanoapp_testing::sendFatalFailureToHost;
@@ -154,7 +151,7 @@ void TimerCancelTest::handleEvent(uint32_t senderInstanceId, uint16_t eventType,
 }
 
 void TimerCancelTest::markSuccess(uint32_t stage) {
-  LOGD("Stage %" PRIu32 " succeeded", stage);
+  chreLog(CHRE_LOG_DEBUG, "Stage %" PRIu32 " succeeded", stage);
   uint32_t finishedBit = (1 << stage);
   if ((kAllFinished & finishedBit) == 0) {
     sendFatalFailureToHost("markSuccess bad stage:", &stage);
