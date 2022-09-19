2117451c090dc85517081e1f7e34f394cd39ae64
openvela-robot <openvela-robot@xiaomi.com>
2022-09-19 19:47:54 +0800

    Fix a stack-use-after-return error am: 4513242890
    Original change: https://android-review.googlesource.com/c/platform/system/chre/+/2220163
    
    Change-Id: Ie9e48a06fea9f6460017e4353dc7d88aa609c78a
    Signed-off-by: Automerger Merge Worker <android-build-automerger-merge-worker@system.gserviceaccount.com>

--- platform/slpi/system_time_util.cc
+++ platform/slpi/system_time_util.cc
@@ -18,21 +18,11 @@
 
 #include "chre/util/time.h"
 
-extern "C" {
-
-#include "uTimetick.h"
-
-}  // extern "C"
-
 namespace chre {
 
-namespace {
-
-const uint64_t kClockFreq = uTimetick_CvtToTicks(1, T_SEC);
-
-}  // anonymous namespace
-
 uint64_t getNanosecondsFromQTimerTicks(uint64_t ticks) {
+  constexpr uint64_t kClockFreq = 19200000;  // 19.2MHz QTimer clock
+
   uint64_t nsec = 0;
   if (ticks >= kClockFreq) {
     uint64_t seconds = (ticks / kClockFreq);
