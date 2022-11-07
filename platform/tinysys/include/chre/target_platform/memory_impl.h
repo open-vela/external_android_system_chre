b9ec3c7a664e2442821a52849a6518581fe51499
openvela-robot <openvela-robot@xiaomi.com>
2022-11-08 02:30:24 +0800

    Snap for 9264775 from d8aabf9ad65f95dd44b4736d3b213ee25d0b25c0 to udc-release
    Change-Id: I8da9a5745ee9fd8d91b976f7bdc0b00887e3eea1

--- platform/tinysys/include/chre/target_platform/memory_impl.h
+++ platform/tinysys/include/chre/target_platform/memory_impl.h
@@ -18,17 +18,8 @@
 #define CHRE_PLATFORM_TINYSYS_MEMORY_H_
 
 #include <cstddef>
-
-#ifdef __cplusplus
-extern "C" {
-#endif
-
 #include "encoding.h"
 #include "sensorhub/heap.h"
-
-#ifdef __cplusplus
-}  // extern "C"
-#endif
 
 namespace chre {
 
