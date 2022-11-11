55d515f5506e8310c2f9c323f0fba8f438d33191
liujinye <liujinye@xiaomi.com>
2022-11-11 23:27:39 +0800

    Create efficient batch removal for SegmentedQueue
    This CL creates a way to remove a batch of elements that matches a
    certain condition efficiently.
    
    Bug: 251925940
    Fixes: 263915792
    Test: unit test
    
    Change-Id: I5d64a57c8965b58b85e86767c608352f4f47dfc4

--- core/event_loop.cc
+++ core/event_loop.cc
@@ -16,6 +16,7 @@
 
 #include "chre/core/event_loop.h"
 #include <cinttypes>
+#include <cstdint>
 
 #include "chre/core/event.h"
 #include "chre/core/event_loop_manager.h"
@@ -28,6 +29,7 @@
 #include "chre/util/conditional_lock_guard.h"
 #include "chre/util/lock_guard.h"
 #include "chre/util/system/debug_dump.h"
+#include "chre/util/system/event_callbacks.h"
 #include "chre/util/system/stats_container.h"
 #include "chre/util/time.h"
 #include "chre_api/chre/version.h"
@@ -56,6 +58,13 @@ bool populateNanoappInfo(const Nanoapp *app, struct chreNanoappInfo *info) {
     info->appId = app->getAppId();
     info->version = app->getAppVersion();
     info->instanceId = app->getInstanceId();
+    if (app->getTargetApiVersion() >= CHRE_API_VERSION_1_8) {
+      CHRE_ASSERT(app->getRpcServices().size() <= Nanoapp::kMaxRpcServices);
+      info->rpcServiceCount =
+          static_cast<uint8_t>(app->getRpcServices().size());
+      info->rpcServices = app->getRpcServices().data();
+      memset(&info->reserved, 0, sizeof(info->reserved));
+    }
     success = true;
   }
 
@@ -244,8 +253,8 @@ void EventLoop::postEventOrDie(uint16_t eventType, void *eventData,
                                uint16_t targetGroupMask) {
   if (mRunning) {
     if (!allocateAndPostEvent(eventType, eventData, freeCallback,
-                              kSystemInstanceId, targetInstanceId,
-                              targetGroupMask)) {
+                              false /*isLowPriority*/, kSystemInstanceId,
+                              targetInstanceId, targetGroupMask)) {
       FATAL_ERROR("Failed to post critical system event 0x%" PRIx16, eventType);
     }
   } else if (freeCallback != nullptr) {
@@ -275,10 +284,14 @@ bool EventLoop::postLowPriorityEventOrFree(
   bool eventPosted = false;
 
   if (mRunning) {
+#if CHRE_STATIC_EVENT_LOOP
     if (mEventPool.getFreeBlockCount() > kMinReservedHighPriorityEventCount) {
-      eventPosted = allocateAndPostEvent(eventType, eventData, freeCallback,
-                                         senderInstanceId, targetInstanceId,
-                                         targetGroupMask);
+#else
+    if (mEventPool.getFreeSpaceCount() > kMinReservedHighPriorityEventCount) {
+#endif
+      eventPosted = allocateAndPostEvent(
+          eventType, eventData, freeCallback, true /*isLowPriority*/,
+          senderInstanceId, targetInstanceId, targetGroupMask);
       if (!eventPosted) {
         LOGE("Failed to allocate event 0x%" PRIx16 " to instanceId %" PRIu16,
              eventType, targetInstanceId);
@@ -359,14 +372,15 @@ void EventLoop::logStateToBuffer(DebugDumpWrapper &debugDump) const {
 
 bool EventLoop::allocateAndPostEvent(uint16_t eventType, void *eventData,
                                      chreEventCompleteFunction *freeCallback,
+                                     bool isLowPriority,
                                      uint16_t senderInstanceId,
                                      uint16_t targetInstanceId,
                                      uint16_t targetGroupMask) {
   bool success = false;
 
   Event *event =
-      mEventPool.allocate(eventType, eventData, freeCallback, senderInstanceId,
-                          targetInstanceId, targetGroupMask);
+      mEventPool.allocate(eventType, eventData, freeCallback, isLowPriority,
+                          senderInstanceId, targetInstanceId, targetGroupMask);
   if (event != nullptr) {
     success = mEvents.push(event);
   }
