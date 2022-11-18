0277a4b1a48efc944fc9272e19578a9f4dfab9b9
liujinye <liujinye@xiaomi.com>
2022-11-19 04:34:55 +0800

    Snap for 9311085 from ade948f169b47865d76f5a5b183e57e01ae9a86a to udc-release
    Change-Id: I21e44b64f2bc0b183e40f131e97c0d1a0d4eead2

--- java/utils/pigweed/src/com/google/android/chre/utils/pigweed/ChreRpcClient.java
+++ java/utils/pigweed/src/com/google/android/chre/utils/pigweed/ChreRpcClient.java
@@ -137,23 +137,21 @@ public class ChreRpcClient {
 
 
     /**
-     * Returns whether the state matches the server nanoapp and the service is provided.
+     * Returns whether the nanoapp supports the service.
      *
-     * @param state           A nanoapp state
-     * @param serverNanoappId The ID of the RPC server nanoapp
-     * @param serviceId       ID of the service
-     * @param serviceVersion  Version of the service
-     * @return the state matches the server nanoapp and the service is provided
+     * @param state   A nanoapp state
+     * @param id      ID of the service
+     * @param version Version of the service
+     * @return whether the nanoapp provides the service at the given ID and version
      */
-    public static boolean hasService(NanoAppState state, long serverNanoappId, long serviceId,
-            int serviceVersion) {
-        if (state.getNanoAppId() != serverNanoappId) {
+    public boolean hasService(NanoAppState state, long id, int version) {
+        if (state.getNanoAppId() != mServerNanoappId) {
             return false;
         }
 
         for (NanoAppRpcService service : state.getRpcServices()) {
-            if (service.getId() == serviceId) {
-                return service.getVersion() == serviceVersion;
+            if (service.getId() == id) {
+                return service.getVersion() == version;
             }
         }
 
