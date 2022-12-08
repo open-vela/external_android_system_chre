f268e82d1e9f5f4a267a7b172f59d3c779acdd62
liujinye <liujinye@xiaomi.com>
2022-12-08 13:49:23 +0800

    Add a client/server RPC simulation test
    Bug: 242301032
    Fix: 217952682
    
    Test: simulation tests
    Change-Id: I30d26ded39be529f0b7dccfa2c2d386e5abdb95c

--- platform/freertos/platform_nanoapp.cc
+++ platform/freertos/platform_nanoapp.cc
@@ -88,7 +88,7 @@ uint32_t PlatformNanoapp::getAppVersion() const {
 
 bool PlatformNanoapp::supportsAppPermissions() const {
   return (mAppInfo != nullptr) ? (mAppInfo->structMinorVersion >=
-                                  CHRE_NSL_NANOAPP_INFO_STRUCT_MINOR_VERSION)
+                                  CHRE_NSL_NANOAPP_INFO_STRUCT_MINOR_VERSION_3)
                                : false;
 }
 
@@ -120,7 +120,7 @@ void PlatformNanoapp::logStateToBuffer(DebugDumpWrapper &debugDump) const {
     size_t versionLen = 0;
     const char *version = getAppVersionString(&versionLen);
     debugDump.print("%s (%s) @ build: %.*s", mAppInfo->name, mAppInfo->vendor,
-                    versionLen, version);
+                    static_cast<int>(versionLen), version);
   }
 }
 
@@ -250,7 +250,7 @@ bool PlatformNanoappBase::verifyNanoappInfo() {
                mAppInfo->appVersionString, mAppInfo->isTcmNanoapp,
                mAppInfo->isSystemNanoapp);
           if (mAppInfo->structMinorVersion >=
-              CHRE_NSL_NANOAPP_INFO_STRUCT_MINOR_VERSION) {
+              CHRE_NSL_NANOAPP_INFO_STRUCT_MINOR_VERSION_3) {
             LOGI("Nanoapp permissions: 0x%" PRIx32, mAppInfo->appPermissions);
           }
         }
