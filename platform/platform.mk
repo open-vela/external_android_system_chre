bea7e83e3089c74f47ef483a0528d0ecb1103f12
openvela-robot <openvela-robot@xiaomi.com>
2022-12-17 04:31:17 +0800

    Snap for 9414774 from 7ff0d225f0449b2b74d5dcf9fc25bf5b85541a56 to udc-release
    Change-Id: Id33302c9fca1560b00a7fd0f7a58c374d49aa769

--- platform/platform.mk
+++ platform/platform.mk
@@ -34,9 +34,6 @@ SLPI_CFLAGS += -Iplatform/slpi/include
 # We use FlatBuffers in the SLPI platform layer
 SLPI_CFLAGS += $(FLATBUFFERS_CFLAGS)
 
-# SLPI still uses static event loop as oppose to heap based dynamic event loop
-SLPI_CFLAGS += -DCHRE_STATIC_EVENT_LOOP
-
 # SLPI/SEE-specific Compiler Flags #############################################
 
 # Include paths.
