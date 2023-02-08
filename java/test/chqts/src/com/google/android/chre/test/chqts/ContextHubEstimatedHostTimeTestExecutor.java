c2036452bb985e59853bd800b9745ee47f5a261e
openvela-robot <openvela-robot@xiaomi.com>
2023-02-09 02:31:11 +0800

    Snap for 9580861 from 7deeb26b1d386ece5890adb63bdb897dc9a17bdf to udc-release
    Change-Id: Idb4d0339ef5a4f943ffc3a851eb95e5086e51354

--- java/test/chqts/src/com/google/android/chre/test/chqts/ContextHubEstimatedHostTimeTestExecutor.java
+++ java/test/chqts/src/com/google/android/chre/test/chqts/ContextHubEstimatedHostTimeTestExecutor.java
@@ -35,10 +35,7 @@ import java.nio.ByteOrder;
  */
 public class ContextHubEstimatedHostTimeTestExecutor extends ContextHubGeneralTestExecutor {
     private static final long MAX_ALLOWED_TIME_DELTA_NS = 10000000;     // 10 ms.
-    private static final int NUM_RTT_SAMPLES = 5;
     private long mMsgSendTimestampNs = 0;
-    private long mSmallestDelta = Long.MAX_VALUE;
-    private int mSamplesReceived = 0;
 
     public ContextHubEstimatedHostTimeTestExecutor(ContextHubManager manager, ContextHubInfo info,
             NanoAppBinary binary) {
@@ -57,30 +54,21 @@ public class ContextHubEstimatedHostTimeTestExecutor extends ContextHubGeneralTe
                 long chreTimestampNs = ByteBuffer.wrap(data)
                         .order(ByteOrder.LITTLE_ENDIAN)
                         .getLong();
+                long halfRtt = (currentTimestampNs - mMsgSendTimestampNs) / 2;
 
-                // Identify the closest CHRE timestamp to the midpoint of RTT.
-                // This needs to be done across multiple rounds since RTT may not
-                // be evenly distributed in a single round
-                long middleTimestamp = (currentTimestampNs - mMsgSendTimestampNs) / 2
-                                       + mMsgSendTimestampNs;
-                long deltaNs = java.lang.Math.abs(chreTimestampNs - middleTimestamp);
+                // The timestamp reported by CHRE must be close to the timestamp
+                // when the message was sent with half the RTT added.
+                long deltaNs = chreTimestampNs - (mMsgSendTimestampNs + halfRtt);
 
-                mSmallestDelta = java.lang.Math.min(mSmallestDelta, deltaNs);
-                mSamplesReceived += 1;
-
-                if (mSamplesReceived == NUM_RTT_SAMPLES) {
-                    if (mSmallestDelta < MAX_ALLOWED_TIME_DELTA_NS) {
-                        pass();
-                    } else {
-                        fail("Inconsistent CHRE/AP timestamps- Current TS: "
-                                + currentTimestampNs + " CHRE TS: " + chreTimestampNs
-                                + " start TS: " + mMsgSendTimestampNs + " Smallest Delta: "
-                                + mSmallestDelta);
-                    }
+                if (java.lang.Math.abs(deltaNs) < MAX_ALLOWED_TIME_DELTA_NS) {
+                    pass();
+                } else {
+                    fail("Inconsistent CHRE/AP timestamps- Current TS: "
+                            + currentTimestampNs + " CHRE TS: " + chreTimestampNs
+                            + " start TS: " + mMsgSendTimestampNs + " Delta: "
+                            + deltaNs);
                 }
-            }
-
-            if (mSamplesReceived < NUM_RTT_SAMPLES) {
+            } else {
                 mMsgSendTimestampNs = SystemClock.elapsedRealtimeNanos();
                 sendMessageToNanoAppOrFail(nanoAppId,
                         ContextHubTestConstants.MessageType.CONTINUE.asInt(),
