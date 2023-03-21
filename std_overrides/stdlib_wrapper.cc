fc18a4e44ce1842c809352e1e4579dfd631c05af
openvela-robot <openvela-robot@xiaomi.com>
2023-03-21 20:24:26 +0800

    [automerger skipped] [RESTRICT AUTOMERGE] Disable flaky CHPP tests am: 8d49eec581 am: db962e4af1 -s ours
    am skip reason: subject contains skip directive
    
    Original change: https://googleplex-android-review.googlesource.com/c/platform/system/chre/+/22181435
    
    Change-Id: Ic3e76d902420c610664fc1a62e7a996fb988922a
    Signed-off-by: Automerger Merge Worker <android-build-automerger-merge-worker@system.gserviceaccount.com>

--- std_overrides/stdlib_wrapper.cc
+++ std_overrides/stdlib_wrapper.cc
@@ -33,8 +33,9 @@
 #include <cstdlib>
 #include <new>
 
-#include <chre.h>
 #include "chre/util/nanoapp/assert.h"
+
+#include "chre_api/chre.h"
 
 #if defined(stderr) && !defined(_CSTD)
 // Provides a definition for stderr when the macro has been defined, but the
@@ -71,6 +72,10 @@ void exit(int exitCode) {
   // execute, since abort terminates the program.
   while (42)
     ;
+}
+
+void abort(void) {
+  exit(CHRE_ERROR);
 }
 
 int fprintf(FILE * /*stream*/, const char * /*fmt*/, ...) {
