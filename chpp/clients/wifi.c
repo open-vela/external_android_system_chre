eb1cee744875c17eb4fd1c8f7cf891ab8a1c13e6
openvela-robot <openvela-robot@xiaomi.com>
2023-04-11 01:52:35 +0800

    Remove flaky assert in WifiNanSubscribeCancel test am: daeb8a6932 am: 03d4b513f3 am: d10e62bde2
    Original change: https://android-review.googlesource.com/c/platform/system/chre/+/2530204
    
    Change-Id: Id4c743e6078cc71a5d2826c786074c8be9e333ca
    Signed-off-by: Automerger Merge Worker <android-build-automerger-merge-worker@system.gserviceaccount.com>

--- chpp/clients/wifi.c
+++ chpp/clients/wifi.c
@@ -46,6 +46,11 @@
 #define CHPP_WIFI_MAX_TIMESYNC_AGE_NS CHPP_TIMESYNC_DEFAULT_MAX_AGE_NS
 #endif
 
+#ifndef CHPP_WIFI_SCAN_RESULT_TIMEOUT_NS
+#define CHPP_WIFI_SCAN_RESULT_TIMEOUT_NS \
+  (CHRE_WIFI_SCAN_RESULT_TIMEOUT_NS - CHRE_NSEC_PER_SEC)
+#endif
+
 /************************************************
  *  Prototypes
  ***********************************************/
@@ -407,7 +412,7 @@ static void chppWifiClientNotifyMatch(void *clientContext) {
 static void chppWiFiRecoverScanMonitor(
     struct ChppWifiClientState *clientContext) {
   if (clientContext->scanMonitorEnabled) {
-    CHPP_LOGI("Re-enabling WiFi scan monitoring after reset");
+    CHPP_LOGD("Re-enabling WiFi scan monitoring after reset");
     clientContext->scanMonitorEnabled = false;
     clientContext->scanMonitorSilenceCallback = true;
 
@@ -553,7 +558,6 @@ static void chppWifiRequestRangingResult(
   struct ChppAppHeader *rxHeader = (struct ChppAppHeader *)buf;
 
   if (rxHeader->error != CHPP_APP_ERROR_NONE) {
-    CHPP_LOGE("Ranging failed at service" PRIu8);
     gCallbacks->rangingEventCallback(chppAppErrorToChreError(rxHeader->error),
                                      NULL);
 
@@ -831,29 +835,31 @@ static bool chppWifiClientOpen(const struct chrePalSystemApi *systemApi,
   CHPP_DEBUG_ASSERT(systemApi != NULL);
   CHPP_DEBUG_ASSERT(callbacks != NULL);
 
-  bool result = false;
   gSystemApi = systemApi;
   gCallbacks = callbacks;
 
   CHPP_LOGD("WiFi client opening");
   if (gWifiClientContext.client.appContext == NULL) {
     CHPP_LOGE("WiFi client app is null");
-  } else {
-    if (chppWaitForDiscoveryComplete(gWifiClientContext.client.appContext,
-                                     CHPP_WIFI_DISCOVERY_TIMEOUT_MS)) {
-      result = chppClientSendOpenRequest(
-          &gWifiClientContext.client,
-          &gWifiClientContext.rRState[CHPP_WIFI_OPEN], CHPP_WIFI_OPEN,
-          /*blocking=*/true);
-    }
-
-    // Since CHPP_WIFI_DEFAULT_CAPABILITIES is mandatory, we can always
-    // pseudo-open and return true. Otherwise, these should have been gated.
-    chppClientPseudoOpen(&gWifiClientContext.client);
-    result = true;
+    return false;
   }
 
-  return result;
+  // Since CHPP_WIFI_DEFAULT_CAPABILITIES is mandatory, we can always
+  // pseudo-open and return true. Otherwise, these should have been gated.
+
+  // Consider the client to be PseudoOpen prior to sending open request
+  // This avoids race conditions where responses could come between
+  // the open request timeout and setting the client as PseudoOpen
+  chppClientPseudoOpen(&gWifiClientContext.client);
+  if (chppWaitForDiscoveryComplete(gWifiClientContext.client.appContext,
+                                   CHPP_WIFI_DISCOVERY_TIMEOUT_MS)) {
+    chppClientSendOpenRequest(&gWifiClientContext.client,
+                              &gWifiClientContext.rRState[CHPP_WIFI_OPEN],
+                              CHPP_WIFI_OPEN,
+                              /*blocking=*/true);
+  }
+
+  return true;
 }
 
 /**
@@ -968,10 +974,14 @@ static bool chppWifiClientRequestScan(const struct chreWifiScanParams *params) {
     request->header.error = CHPP_APP_ERROR_NONE;
     request->header.command = CHPP_WIFI_REQUEST_SCAN_ASYNC;
 
+    CHPP_STATIC_ASSERT(
+        CHRE_WIFI_SCAN_RESULT_TIMEOUT_NS > CHPP_WIFI_SCAN_RESULT_TIMEOUT_NS,
+        "Chpp wifi scan timeout needs to be smaller than CHRE wifi scan "
+        "timeout");
     result = chppSendTimestampedRequestOrFail(
         &gWifiClientContext.client,
         &gWifiClientContext.rRState[CHPP_WIFI_REQUEST_SCAN_ASYNC], request,
-        requestLen, CHRE_WIFI_SCAN_RESULT_TIMEOUT_NS);
+        requestLen, CHPP_WIFI_SCAN_RESULT_TIMEOUT_NS);
   }
 
   return result;
@@ -1152,11 +1162,19 @@ static bool chppWifiClientNanRequestNanRanging(
   return result;
 }
 
+static bool chppWifiGetNanCapabilites(
+    struct chreWifiNanCapabilities *capabilities) {
+  // Not implemented yet.
+  UNUSED_VAR(capabilities);
+  return false;
+}
+
 /************************************************
  *  Public Functions
  ***********************************************/
 
 void chppRegisterWifiClient(struct ChppAppState *appContext) {
+  memset(&gWifiClientContext, 0, sizeof(gWifiClientContext));
   chppRegisterClient(appContext, (void *)&gWifiClientContext,
                      &gWifiClientContext.client, gWifiClientContext.rRState,
                      &kWifiClientConfig);
@@ -1194,6 +1212,7 @@ const struct chrePalWifiApi *chppPalWifiGetApi(uint32_t requestedApiVersion) {
       .nanSubscribeCancel = chppWifiClientNanSubscribeCancel,
       .releaseNanDiscoveryEvent = chppWifiClientNanReleaseDiscoveryEvent,
       .requestNanRanging = chppWifiClientNanRequestNanRanging,
+      .getNanCapabilities = chppWifiGetNanCapabilites,
   };
 
   CHPP_STATIC_ASSERT(
