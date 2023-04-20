1491641a705688b38a15cb90bede9b67f115e03f
liujinye <liujinye@xiaomi.com>
2023-04-20 23:31:54 +0800

    Snap for 9981223 from e3b63a45f92d0fe35f2498cee79d44416d65c21a to udc-release
    Change-Id: I4390ac1453718063f93294c7dba002aa7dad1f94

--- platform/tinysys/host_link.cc
+++ platform/tinysys/host_link.cc
@@ -564,12 +564,14 @@ void HostLinkBase::sendLogMessageV2(const uint8_t *logMessage,
   };
 
   constexpr size_t kInitialSize = 128;
-  bool result = false;
+  bool result = true;
   if (isInitialized()) {
     result = buildAndEnqueueMessage(
         PendingMessageType::EncodedLogMessage,
         kInitialSize + logMessageSize + sizeof(numLogsDropped), msgBuilder,
         &logMessageData);
+  } else {
+    LOGW("Dropping outbound message: host link not initialized yet");
   }
 
 #ifdef CHRE_USE_BUFFERED_LOGGING
