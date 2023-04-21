db032089050708e66494ca8d8551f757185394f3
liujinye <liujinye@xiaomi.com>
2023-04-22 01:35:19 +0800

    Snap for 9990577 from 8398092254f727e40727d64259869d155d3717db to udc-release
    Change-Id: I74782919c393cdfa8090984b5a5d62f882cb685a

--- host/hal_generic/common/multi_client_context_hub_base.cc
+++ host/hal_generic/common/multi_client_context_hub_base.cc
@@ -340,18 +340,14 @@ ScopedAStatus MultiClientContextHubBase::onHostEndpointConnected(
 ScopedAStatus MultiClientContextHubBase::onHostEndpointDisconnected(
     char16_t in_hostEndpointId) {
   HostEndpointId hostEndpointId = in_hostEndpointId;
-  bool isSuccessful = false;
-  if (mHalClientManager->removeEndpointId(hostEndpointId) &&
-      mHalClientManager->mutateEndpointIdFromHostIfNeeded(
+  if (!mHalClientManager->removeEndpointId(hostEndpointId) ||
+      !mHalClientManager->mutateEndpointIdFromHostIfNeeded(
           AIBinder_getCallingPid(), hostEndpointId)) {
-    flatbuffers::FlatBufferBuilder builder(64);
-    HostProtocolHost::encodeHostEndpointDisconnected(builder, hostEndpointId);
-    isSuccessful = mConnection->sendMessage(builder);
+    return ScopedAStatus::fromExceptionCode(EX_ILLEGAL_ARGUMENT);
   }
-  if (!isSuccessful) {
-    LOGW("Unable to remove host endpoint id %" PRIu16, in_hostEndpointId);
-  }
-  return ScopedAStatus::ok();
+  flatbuffers::FlatBufferBuilder builder(64);
+  HostProtocolHost::encodeHostEndpointDisconnected(builder, hostEndpointId);
+  return fromResult(mConnection->sendMessage(builder));
 }
 
 ScopedAStatus MultiClientContextHubBase::onNanSessionStateChanged(
