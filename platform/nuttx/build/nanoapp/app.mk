f23ca5368599f2bdd1c1b82049af1cb0fd405829
wangjianyu3 <wangjianyu3@xiaomi.com>
2024-07-03 10:40:43 +0800

    external/chre: Generate current_chre_api_version.mk for napp_header
    Signed-off-by: wangjianyu3 <wangjianyu3@xiaomi.com>

--- platform/nuttx/build/nanoapp/app.mk
+++ platform/nuttx/build/nanoapp/app.mk
@@ -1,5 +1,6 @@
 include $(APPDIR)/Make.defs
 CXXEXT = .cc
+OUT=$(shell pwd)/out
 HEADER_SUFFIX = ".napp_header"
 
 ANDROID_BUILD_TOP = $(APPDIR)/external/android
@@ -12,27 +13,33 @@ include $(PATH_CHRE)/build/variant/nuttx.mk
 
 # Nanoapp Header
 #   From: build/build_template.mk
-include $(shell pwd)/out/current_chre_api_version.mk
-ifeq ($(CHRE_API_VERSION_MAJOR),)
-TARGET_CHRE_API_VERSION_MAJOR = $(DEFAULT_CHRE_API_VERSION_MAJOR)
-endif
-ifeq ($(CHRE_API_VERSION_MINOR),)
-TARGET_CHRE_API_VERSION_MINOR = $(DEFAULT_CHRE_API_VERSION_MINOR)
-endif
+CHRE_HOST_CC ?= $(HOSTCC)
 TARGET_NANOAPP_FLAGS ?= 0x00000001
 BE_TO_LE_SCRIPT = $(PATH_CHRE)/build/be_to_le.sh
 
-# Nanoapp Header
-#   From: build/build_template.mk
-include $(shell pwd)/out/current_chre_api_version.mk
+PRINT_CURRENT_CHRE_API_VERSION_SRCS = $(PATH_CHRE)/build/print_current_chre_api_version.c
+PRINT_CURRENT_CHRE_API_VERSION_BIN = $(OUT)/print_current_chre_api_version
+CURRENT_CHRE_API_VERSION_MK = $(OUT)/current_chre_api_version.mk
+
+$(PRINT_CURRENT_CHRE_API_VERSION_BIN): $(PRINT_CURRENT_CHRE_API_VERSION_SRCS)
+	$(Q)mkdir -p $(OUT)
+	$(Q)$(CHRE_HOST_CC) -I$(PATH_CHRE)/chre_api/include/chre_api $^ -o $@
+
+$(CURRENT_CHRE_API_VERSION_MK): $(PRINT_CURRENT_CHRE_API_VERSION_BIN)
+	$(Q)$< > $@
+
+# Only include default version if this is not a clean operation.
+ifeq ($(filter clean, $(MAKECMDGOALS)),)
+-include $(CURRENT_CHRE_API_VERSION_MK)
+endif
+
 ifeq ($(CHRE_API_VERSION_MAJOR),)
 TARGET_CHRE_API_VERSION_MAJOR = $(DEFAULT_CHRE_API_VERSION_MAJOR)
 endif
+
 ifeq ($(CHRE_API_VERSION_MINOR),)
 TARGET_CHRE_API_VERSION_MINOR = $(DEFAULT_CHRE_API_VERSION_MINOR)
 endif
-TARGET_NANOAPP_FLAGS ?= 0x00000001
-BE_TO_LE_SCRIPT = $(PATH_CHRE)/build/be_to_le.sh
 
 ifneq ($(V), 0)
 CHRE_BUILD_VERBOSE = true
